de4ef2e59a8a13daed19ee025d9e77a0a93a359d
Irina Hristova <hristova119@gmail.com>
2017-10-19 20:12:25 +0300

    Solving problems.

--- TAAExam13Oct2017/02. PrimeTriangle/PrimeTriangle.cs
+++ TAAExam13Oct2017/02. PrimeTriangle/PrimeTriangle.cs
@@ -1,16 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class PrimeTriangle
 {
     static void Main()
     {
         int n = int.Parse(Console.ReadLine());
+        int counter = 0;
+        int boundary = (int)(Math.Floor(Math.Sqrt(n)));
+        List<int> primes = new List<int>(2);
+        primes[0] = 1;
+        primes[1] = 2;
+        if (n < 3)
+        {
 
-    }
-
-    static void FindPrimeNumbers(int number)
-    {
-
+        }
+        else
+        {
+            for (int i = 3; i <= boundary; i += 2)
+            {
+                if(n % i == 0)
+                {
+                    primes.Add(i);
+                }
+            }
+        }
     }
 }
 
