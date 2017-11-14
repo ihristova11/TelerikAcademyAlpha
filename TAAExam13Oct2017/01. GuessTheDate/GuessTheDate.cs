987544e02a988efcbfa57c45d9a24407c0ecc60b
Irina Hristova <hristova119@gmail.com>
2017-11-14 16:04:54 +0200

    Solving problem.

--- TAAExam13Oct2017/01. GuessTheDate/GuessTheDate.cs
+++ TAAExam13Oct2017/01. GuessTheDate/GuessTheDate.cs
@@ -7,53 +7,7 @@ class GuessTheDate
         int year = int.Parse(Console.ReadLine());
         int month = int.Parse(Console.ReadLine());
         int day = int.Parse(Console.ReadLine());
-        string strMonth = "";
-        if (day == 1)
-        {
 
-        }
-        switch (month)
-        {
-            case 1:
-                strMonth += "Jan";
-                break;
-            case 2:
-                strMonth += "Feb";
-                break;
-            case 3:
-                strMonth += "Mar";
-                break;
-            case 4:
-                strMonth += "Apr";
-                break;
-            case 5:
-                strMonth += "May";
-                break;
-            case 6:
-                strMonth += "Jun";
-                break;
-            case 7:
-                strMonth += "Jul";
-                break;
-            case 8:
-                strMonth += "Aug";
-                break;
-            case 9:
-                strMonth += "Sep";
-                break;
-            case 10:
-                strMonth += "Oct";
-                break;
-            case 11:
-                strMonth += "Nov";
-                break;
-            case 12:
-                strMonth += "Dec";
-                break;
-            default:
-                break;
-        }
-        Console.WriteLine(day - 1 + "-" + strMonth + "-" + year);
     }
-    }
+}
 
