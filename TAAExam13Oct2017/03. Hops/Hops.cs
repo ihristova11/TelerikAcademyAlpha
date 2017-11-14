b0102cf6623c794877533382351086dc3a76dde6
Irina Hristova <hristova119@gmail.com>
2017-11-14 17:42:50 +0200

    Solving problem.

--- TAAExam13Oct2017/03. Hops/Hops.cs
+++ TAAExam13Oct2017/03. Hops/Hops.cs
@@ -6,6 +6,14 @@
     {
         static void Main()
         {
+            // good > 0
+            // bad < 0
+
+            // read the input string
+            string line = Console.ReadLine();
+            string[] field = line.Split(',');
+
+
         }
     }
 }
