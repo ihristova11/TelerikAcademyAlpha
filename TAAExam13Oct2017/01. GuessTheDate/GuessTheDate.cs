302495ba0ecf4d370123907a67726b74e4582b5a
Irina Hristova <hristova119@gmail.com>
2017-11-14 16:32:52 +0200

    Solving problem.

--- TAAExam13Oct2017/01. GuessTheDate/GuessTheDate.cs
+++ TAAExam13Oct2017/01. GuessTheDate/GuessTheDate.cs
@@ -10,7 +10,7 @@ class GuessTheDate
 
         string[] months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
         int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-        string output = "";
+        //string output = "";
 
         // jan - 31 feb - 30 march - 31 apr - 30 may - 31, june - 30 
         // july - 31, aug - 31, sept - 30, oct - 31, nov - 30, dec - 31
@@ -18,10 +18,11 @@ class GuessTheDate
         //check for first day of month
         if (day == 1)
         {
-            // for Feb in harvest year
+            
             if (month != 1)
             {
                 month = month - 1;
+                // for Feb in harvest year
                 if (year % 4 == 0 && month == 2)
                 {
                     day = days[month + 1] + 1;
@@ -31,7 +32,20 @@ class GuessTheDate
                     day = days[month + 1];
                 }
             }
+            else // DECEMBER case
+            {
+                month = 11;
+                day = 31;
+                year = year - 1;
+            }
         }
+        else 
+        {
+            month = month - 1;
+            day = day - 1;
+        }
+        //output += "{0}-{1}-{2}", day, months[month + 1], year;
+        Console.WriteLine("{0}-{1}-{2}", day, months[month - 1], year);
     }
 }
 
