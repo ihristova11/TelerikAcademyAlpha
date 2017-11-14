1a68a8bc7b504398d63febafbfb547c2154fa678
Irina Hristova <hristova119@gmail.com>
2017-11-14 16:51:10 +0200

    Solving problem.

--- TAAExam13Oct2017/02. PrimeTriangle/PrimeTriangle.cs
+++ TAAExam13Oct2017/02. PrimeTriangle/PrimeTriangle.cs
@@ -1,11 +1,34 @@
 ﻿namespace _02.PrimeTriangle
 {
     using System;
+    using System.Text;
 
     class PrimeTriangle
     {
         static void Main()
         {
+
+        }
+
+        // method for finding prime number
+        static void FindPrimeNumber(int n)
+        {
+            int i = 2;
+            while(n != 0)
+            {
+                n = n / i;
+                i++;
+            }
+        }
+
+        // method for printing all numbers in a line
+        static void PrintLine(int num)
+        {
+            StringBuilder line = new StringBuilder();
+            for (int i = 1; i <= num; i++)
+            {
+                line = line.Append(i);
+            }
         }
     }
 }
