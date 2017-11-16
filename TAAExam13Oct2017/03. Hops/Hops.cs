67097d6489c8a3e45bcf6490a54555f22a386259
Irina Hristova <hristova119@gmail.com>
2017-11-16 14:28:36 +0200

    Solving problem.

--- TAAExam13Oct2017/03. Hops/Hops.cs
+++ TAAExam13Oct2017/03. Hops/Hops.cs
@@ -5,45 +5,59 @@
     class Hops
     {
         static string[] field;
+        static int maxSum = 0;
         static int currSum = 0;
+
         static void Main()
         {
-            // good > 0
-            // bad < 0
-            int maxSum = int.MinValue;
             // read the input string
             string line = Console.ReadLine();
             field = line.Split(',');
+
+            //read the number of instructions
             int m = int.Parse(Console.ReadLine());
+
+            //read all the different lines of instructions
             for (int j = 0; j < m; j++)
             {
+                // read the instructions from the console
                 string directions = Console.ReadLine();
                 string[] directionsArray = directions.Split(',');
 
-                int sum = int.Parse(field[0]);
-                int position = 0;
-
-                for (int i = 0; i < directionsArray.Length; i++)
-                {
-                    while (position >= 0 && position < directionsArray.Length)
-                    {
-                        if(i >= directionsArray.Length && position >= 0 && position < directionsArray.Length)
-                        {
-                            i = 0;
-                        }
-                        position += int.Parse(directionsArray[i]);
-
-                        sum += int.Parse(field[position]);
-
-                        if (maxSum < sum)
-                        {
-                            maxSum = sum;
-                        }
-                    }
-                }
+               
             }
 
             Console.WriteLine(maxSum);
+        }
+
+        // method for hopping through the array(field)
+        static void CalculateSum(string[] field, string[] directionsArray)
+        {
+            int sum = int.Parse(field[0]);
+            field[0] = "a";
+            int position = 0;
+
+            for (int i = 0; i < directionsArray.Length; i++)
+            {
+                position += int.Parse(directionsArray[i]);
+
+                if(position >= field.Length || position < 0 || field[position] == "a")
+                {
+                    break;
+                }
+                else
+                {
+                    sum += int.Parse(field[position]);
+                }
+            }
+            currSum = sum;
+        }
+
+        static int MaxSum(int currSum, int maxSum)
+        {
+            maxSum = maxSum < currSum ? currSum : maxSum;
+
+            return maxSum;
         }
     }
 }
