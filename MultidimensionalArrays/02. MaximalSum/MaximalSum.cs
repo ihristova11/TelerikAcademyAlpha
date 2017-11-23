da93d972d1d18b7c0f1341ff72ab8f645993ccba
Irina Hristova <hristova119@gmail.com>
2017-11-23 23:17:34 +0200

    Solving problem.

--- MultidimensionalArrays/02. MaximalSum/MaximalSum.cs
+++ MultidimensionalArrays/02. MaximalSum/MaximalSum.cs
@@ -10,14 +10,16 @@
         public static int currSum = 0;
         public static int row = 0;
         public static int col = 0;
+        public static int rows;
+        public static int cols;
 
         static void Main()
         {
             //reading the input
             string line = Console.ReadLine();
             int[] input = line.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-            int rows = input[0];
-            int cols = input[1];
+            rows = input[0];
+            cols = input[1];
 
             array = new int[rows][];
 
@@ -27,18 +29,31 @@
                 array[i] = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
             }
 
+            for (int i = 0; i < rows - 3; i++)
+            {
+                for (int j = 0; j < cols - 3; j++)
+                {
+                    row = i;
+                    col = j;
+
+                    CalculateSum();
+                }
+            }
+            Console.WriteLine(maxSum);
         }
 
         public static void CalculateSum()
         {
-            for (; row < row + 3; row++)
+            for (; row <= row + 3; row++)
             {
-                for (; col < col + 3; col++)
+                for (; col <= col + 3; col++)
                 {
                     currSum += array[row][col];
                 }
             }
 
+            maxSum = maxSum < currSum ? currSum : maxSum;
+            currSum = 0;
         }
     }
 }
