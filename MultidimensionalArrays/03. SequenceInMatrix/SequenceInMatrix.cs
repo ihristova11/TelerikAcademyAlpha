c3d0d7075f2163813194100390e5778d216b5acb
Irina Hristova <hristova119@gmail.com>
2017-11-24 00:06:18 +0200

    Solving problem.

--- MultidimensionalArrays/03. SequenceInMatrix/SequenceInMatrix.cs
+++ MultidimensionalArrays/03. SequenceInMatrix/SequenceInMatrix.cs
@@ -1,11 +1,29 @@
 ﻿namespace _03.SequenceInMatrix
 {
     using System;
+    using System.Linq;
 
     class SequenceInMatrix
     {
+        public static int rows;
+        public static int cols;
+        public static int[][] array;
         static void Main()
         {
+            //reading the input
+            string line = Console.ReadLine();
+            int[] input = line.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            rows = input[0];
+            cols = input[1];
+
+            array = new int[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                array[i] = new int[cols];
+                array[i] = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            }
+
         }
     }
 }
