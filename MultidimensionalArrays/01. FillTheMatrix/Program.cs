8b81b7ae20f461cebf037e1994b285b83d06baf1
Irina Hristova <hristova119@gmail.com>
2017-11-23 22:05:14 +0200

    Solved problem.

--- MultidimensionalArrays/01. FillTheMatrix/Program.cs
+++ MultidimensionalArrays/01. FillTheMatrix/Program.cs
@@ -4,116 +4,30 @@
 
     class Program
     {
+        public static int counter = 1;
+        public static int rows;
+        public static int cols;
         static void Main()
         {
             int n = int.Parse(Console.ReadLine());
             char type = char.Parse(Console.ReadLine());
             int[,] matrix = new int[n, n];
-            int rows;
-            int cols;
-            int counter = 1;
 
             switch (type)
             {
                 //working
                 case 'a':
-                    matrix[0, 0] = counter;
-
-                    for (cols = 0; cols < n; cols++)
-                    {
-                        for (rows = 0; rows < n; rows++)
-                        {
-                            matrix[rows, cols] = counter;
-                            counter++;
-                        }
-                    }
-
-                    PrintTheMatrix(matrix, n);
-
+                    CalculateCaseA(matrix, n);
                     break;
 
                 //working
                 case 'b':
-                    matrix[0, 0] = counter;
-
-                    for (cols = 0; cols < n; cols++)
-                    {
-                        for (rows = 0; rows < n; rows++)
-                        {
-                            if (cols % 2 == 0)
-                            {
-                                matrix[rows, cols] = counter;
-                            }
-                            else
-                            {
-                                matrix[n - rows - 1, cols] = counter;
-                            }
-                            counter++;
-                        }
-                    }
-
-                    PrintTheMatrix(matrix, n);
-
+                    CalculateCaseB(matrix, n);
                     break;
 
 
                 case 'c':
-
-                    counter = 1;
-                    cols = 0; rows = n - 1;
-                    matrix[rows, cols] = counter;
-                    counter++;
-                    int downB;
-
-                    for (int i = 2; i <= n; i++)
-                    {
-                        cols = 0;
-                        rows = n - i;
-                        downB = i - 1;
-
-                        while (downB != 0)
-                        {
-                            matrix[rows, cols] = counter;
-                            cols++;
-                            rows++;
-                            counter++;
-                            downB--;
-                            if (rows == n - 1)
-                            {
-                                matrix[rows, cols] = counter;
-                                counter++;
-                            }
-                        }
-                    }
-
-                    for (int j = n - 1; j >= 0; j--)
-                    {
-                        rows = 0;
-                        cols = n - j;
-                        downB = j - 1;
-
-                        while (downB > 0)
-                        {
-                            matrix[rows, cols] = counter;
-                            cols++;
-                            rows++;
-                            counter++;
-                            downB--;
-                            if (cols == n - 1)
-                            {
-                                matrix[rows, cols] = counter;
-                                counter++;
-                            }
-                        }
-
-                        if (rows == 0 && cols == n - 1)
-                        {
-                            matrix[rows, cols] = counter;
-                        }
-                    }
-
-                    PrintTheMatrix(matrix, n);
-
+                    CalculateCaseC(matrix, n);
                     break;
 
 
@@ -174,12 +88,145 @@
                     break;
             }
         }
-
-
-        public static void SetMatrixElement(int[,] matrix, int rows, int cols, int counter)
+        
+        public static void CalculateCaseD(int[,] matrix, int n)
         {
+            rows = 0;
+            cols = 0;
+            counter = 1;
+            matrix[rows, cols] = counter;
+
+            for (int bound = 0; bound < n / 2 + 1; bound++)
+            {
+                rows = bound;
+                cols = bound;
+                for (; rows < n - bound; rows++)
+                {
+                    if (rows == n - bound - 1)
+                    {
+                        for (; cols < n - bound; cols++)
+                        {
+                            matrix[rows, cols] = counter;
+                            counter++;
+                        }
+                    }
+                    else
+                    {
+                        matrix[rows, cols] = counter;
+                        counter++;
+                    }
+                }
+                cols = n - bound - 1;
+                rows = n - bound - 2;
+                for (; rows >= bound; rows--)
+                {
+
+                    if (rows == bound)
+                    {
+                        for (cols = n - bound - 1; cols >= bound + 1; cols--)
+                        {
+                            matrix[rows, cols] = counter;
+                            counter++;
+                        }
+                    }
+                    else
+                    {
+                        matrix[rows, cols] = counter;
+                        counter++;
+                    }
+                }
+            }
+            PrintTheMatrix(matrix, n);
+        }
+
+        public static void CalculateCaseC(int[,] matrix, int n)
+        {
+            counter = 1;
+            cols = 0; rows = n - 1;
             matrix[rows, cols] = counter;
             counter++;
+            int downB;
+
+            for (int i = 2; i <= n; i++)
+            {
+                cols = 0;
+                rows = n - i;
+                downB = i - 1;
+
+                while (downB != 0)
+                {
+                    matrix[rows, cols] = counter;
+                    cols++;
+                    rows++;
+                    counter++;
+                    downB--;
+                    if (rows == n - 1)
+                    {
+                        matrix[rows, cols] = counter;
+                        counter++;
+                    }
+                }
+            }
+            for (int j = n - 1; j >= 0; j--)
+            {
+                rows = 0;
+                cols = n - j;
+                downB = j - 1;
+                while (downB > 0)
+                {
+                    matrix[rows, cols] = counter;
+                    cols++;
+                    rows++;
+                    counter++;
+                    downB--;
+                    if (cols == n - 1)
+                    {
+                        matrix[rows, cols] = counter;
+                        counter++;
+                    }
+                }
+                if (rows == 0 && cols == n - 1)
+                {
+                    matrix[rows, cols] = counter;
+                }
+            }
+            PrintTheMatrix(matrix, n);
+        }
+
+        public static void CalculateCaseB(int[,] matrix, int n)
+        {
+            matrix[0, 0] = counter;
+            for (cols = 0; cols < n; cols++)
+            {
+                for (rows = 0; rows < n; rows++)
+                {
+                    if (cols % 2 == 0)
+                    {
+                        matrix[rows, cols] = counter;
+                    }
+                    else
+                    {
+                        matrix[n - rows - 1, cols] = counter;
+                    }
+                    counter++;
+                }
+            }
+            PrintTheMatrix(matrix, n);
+        }
+
+        public static void CalculateCaseA(int[,] matrix, int n)
+        {
+            matrix[0, 0] = counter;
+
+            for (cols = 0; cols < n; cols++)
+            {
+                for (rows = 0; rows < n; rows++)
+                {
+                    matrix[rows, cols] = counter;
+                    counter++;
+                }
+            }
+            PrintTheMatrix(matrix, n);
         }
 
         public static void PrintTheMatrix(int[,] matrix, int n)
