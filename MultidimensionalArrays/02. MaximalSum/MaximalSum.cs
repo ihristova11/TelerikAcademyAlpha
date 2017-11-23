9024e5b96a0fb77b8bbe81dff171045f694f0f28
Irina Hristova <hristova119@gmail.com>
2017-11-23 23:03:39 +0200

    Solving problem.

--- MultidimensionalArrays/02. MaximalSum/MaximalSum.cs
+++ MultidimensionalArrays/02. MaximalSum/MaximalSum.cs
@@ -6,6 +6,10 @@
     class MaximalSum
     {
         public static int[][] array;
+        public static int maxSum = int.MinValue;
+        public static int currSum = 0;
+        public static int row = 0;
+        public static int col = 0;
 
         static void Main()
         {
@@ -15,11 +19,26 @@
             int rows = input[0];
             int cols = input[1];
 
+            array = new int[rows][];
+
             for (int i = 0; i < rows; i++)
             {
                 array[i] = new int[cols];
                 array[i] = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
             }
+
+        }
+
+        public static void CalculateSum()
+        {
+            for (; row < row + 3; row++)
+            {
+                for (; col < col + 3; col++)
+                {
+                    currSum += array[row][col];
+                }
+            }
+
         }
     }
 }
