5485ec8555b09d4c23bac27a7f59cb408f1ff5ff
Irina Hristova <hristova119@gmail.com>
2017-11-24 11:51:01 +0200

    Solving problem.

--- MultidimensionalArrays/03. SequenceInMatrix/SequenceInMatrix.cs
+++ MultidimensionalArrays/03. SequenceInMatrix/SequenceInMatrix.cs
@@ -29,7 +29,7 @@
             CalculateLine();
             CalculateColumn();
             //CalculateDiagonalLeft();
-            //CalculateDiagonalRight();
+            CalculateDiagonalRight();
             Console.WriteLine(maxLen);
 
         }
@@ -78,84 +78,162 @@
         public static void CalculateDiagonalRight()
         {
             currLen = 1;
-            for (int i = 0, j = 0; j < cols;)
+            //for (int i = 0, j = 0; j < cols; i++)
+            //{
+            //    for (int startR = i, startC = j; ;)
+            //    {
+            //        if (((startR + 1) < rows) && ((startC + 1) < cols))
+            //        {
+            //            startR++;
+            //            startC++;
+
+            //            if (array[startR][startC] == array[startR - 1][startC - 1])
+            //            {
+            //                currLen++;
+            //            }
+            //            else
+            //            {
+            //                currLen = 1;
+            //            }
+            //            maxLen = maxLen < currLen ? currLen : maxLen;
+            //        }
+            //        else
+            //        {
+            //            break;
+            //        }
+            //    }
+
+            //    if (i != 0)
+            //    {
+            //        i++;
+            //    }
+            //    else
+            //    {
+            //        j++;
+            //    }
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    for (int j = 0; j < cols; j++)
+            //    {
+            //        for (int x = i, y = j; x < rows && y < cols; x++, y++)
+            //        {
+            //            if (array[x][y] == array[x - 1][y - 1])
+            //            {
+            //                currLen++;
+            //            }
+            //            else
+            //            {
+            //                currLen = 1;
+            //            }
+            //        }
+
+            //        for (int x = i, y = cols - 1; x < rows && y >= 0; x++, y--)
+            //        {
+            //            if (array[x][y] == array[x - 1][y - 1])
+            //            {
+            //                currLen++;
+            //            }
+            //            else
+            //            {
+            //                currLen = 1;
+            //            }
+            //        }
+            //    }
+            //}
+            
+            cols = 0; rows--;
+            array[rows][cols] = counter;
+            int downB;
+
+            for (int i = 2; i <= n; i++)
             {
-                for (int startR = i, startC = j; ;)
-                {
-                    if (((startR + 1) < rows) && ((startC + 1) < cols))
-                    {
-                        startR++;
-                        startC++;
+                cols = 0;
+                rows = n - i;
+                downB = i - 1;
 
-                        if (array[startR][startC] == array[startR - 1][startC - 1])
-                        {
-                            currLen++;
-                        }
-                        else
-                        {
-                            currLen = 1;
-                        }
-                        maxLen = maxLen < currLen ? currLen : maxLen;
-                    }
-                    else
+                while (downB != 0)
+                {
+                    matrix[rows, cols] = counter;
+                    cols++;
+                    rows++;
+                    counter++;
+                    downB--;
+                    if (rows == n - 1)
                     {
-                        break;
+                        matrix[rows, cols] = counter;
+                        counter++;
                     }
                 }
-
-                if (i != 0)
-                {
-                    i--;
-                }
-                else
-                {
-                    j++;
-                }
-
-                maxLen = maxLen < currLen ? currLen : maxLen;
-                currLen = 0;
             }
+            for (int j = n - 1; j >= 0; j--)
+            {
+                rows = 0;
+                cols = n - j;
+                downB = j - 1;
+                while (downB > 0)
+                {
+                    matrix[rows, cols] = counter;
+                    cols++;
+                    rows++;
+                    counter++;
+                    downB--;
+                    if (cols == n - 1)
+                    {
+                        matrix[rows, cols] = counter;
+                        counter++;
+                    }
+                }
+                if (rows == 0 && cols == n - 1)
+                {
+                    matrix[rows, cols] = counter;
+                }
+            }
+
+
+            maxLen = maxLen < currLen ? currLen : maxLen;
+                currLen = 0;
+            
         }
 
-        public static void CalculateDiagonalLeft()
-        {
-            for (int i = rows - 1, j = cols - 1; j >= 0;)
-            {
-                for (int startR = i, startC = j; ;)
-                {
-                    if (((startR + 1) < rows) && ((startC + 1) < cols))
-                    {
-                        startR++;
-                        startC++;
+        //public static void CalculateDiagonalLeft()
+        //{
+        //    for (int i = rows - 1, j = cols - 1; j >= 0;)
+        //    {
+        //        for (int startR = i, startC = j; ;)
+        //        {
+        //            if (((startR + 1) < rows) && ((startC + 1) < cols))
+        //            {
+        //                startR++;
+        //                startC++;
 
-                        if (array[startC][startR] == array[startC - 1][startR - 1])
-                        {
-                            currLen++;
-                        }
-                        else
-                        {
-                            maxLen = maxLen < currLen ? currLen : maxLen;
-                            currLen = 0;
-                        }
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
+        //                if (array[startC][startR] == array[startC - 1][startR - 1])
+        //                {
+        //                    currLen++;
+        //                }
+        //                else
+        //                {
+        //                    maxLen = maxLen < currLen ? currLen : maxLen;
+        //                    currLen = 0;
+        //                }
+        //            }
+        //            else
+        //            {
+        //                break;
+        //            }
+        //        }
 
-                if (i != 0)
-                {
-                    i--;
-                }
-                else
-                {
-                    j++;
-                }
+        //        if (i != 0)
+        //        {
+        //            i--;
+        //        }
+        //        else
+        //        {
+        //            j++;
+        //        }
 
-                maxLen = maxLen < currLen ? currLen : maxLen;
-                currLen = 0;
-            }
-        }
+        //        maxLen = maxLen < currLen ? currLen : maxLen;
+        //        currLen = 0;
+        //    }
+        //}
     }
 }
