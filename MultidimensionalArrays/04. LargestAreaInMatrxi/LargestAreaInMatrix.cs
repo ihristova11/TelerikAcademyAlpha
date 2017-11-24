eeda34e6415f020bfba6d03210c6860d3db4360d
Irina Hristova <hristova119@gmail.com>
2017-11-24 12:07:07 +0200

    Solving problem.

--- MultidimensionalArrays/04. LargestAreaInMatrxi/LargestAreaInMatrix.cs
+++ MultidimensionalArrays/04. LargestAreaInMatrxi/LargestAreaInMatrix.cs
@@ -1,11 +1,28 @@
 ﻿namespace _04.LargestAreaInMatrxi
 {
     using System;
-
+    using System.Linq;
     class LargestAreaInMatrix
     {
+        public static int rows;
+        public static int cols;
+        public static int[][] array;
         static void Main()
         {
+            // reading the input
+            //reading the input
+            string line = Console.ReadLine();
+            int[] input = line.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            rows = input[0];
+            cols = input[1];
+
+            array = new int[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                array[i] = new int[cols];
+                array[i] = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            }
         }
     }
 }
