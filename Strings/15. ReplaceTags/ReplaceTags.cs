55dd9e3ed98a847b9ae5a633e92d4ebad110855d
Irina Hristova <hristova119@gmail.com>
2017-11-25 20:55:39 +0200

    Solved problem.

--- Strings/15. ReplaceTags/ReplaceTags.cs
+++ Strings/15. ReplaceTags/ReplaceTags.cs
@@ -2,32 +2,38 @@
 {
     using System;
     using System.Text;
+    using System.Text.RegularExpressions;
 
     class ReplaceTags
     {
-        static void Main()
-        {
-            string input = Console.ReadLine();
-            StringBuilder sb = new StringBuilder(input);
-            string url = "";
-            string text = "";
-            string oldStr = "";
-            int index = 0;
-            string newStr = "";
 
-            while (input.IndexOf("href") != -1)
-            {
-                input = input.Remove(0, input.IndexOf("href") + "href=".Length + 1);
-                url = input.Substring(0, input.IndexOf(">") - 1);
-                input = input.Remove(0, url.Length + 2);
-                text = input.Substring(0, input.IndexOf("</a>"));
-                oldStr = "<a href=\"" + url + "\">" + text + "</а>";
-                newStr = "[" + text + "](" + url + ")";
-                index = sb.ToString().IndexOf("<a");
-                sb = sb.Remove(index, oldStr.Length);
-                sb = sb.Insert(index, newStr);
-            }
-            Console.WriteLine(sb);
+        //string input = Console.ReadLine();
+        //StringBuilder sb = new StringBuilder(input);
+        //string url = "";
+        //string text = "";
+        //string oldStr = "";
+        //int index = 0;
+        //string newStr = "";
+
+        //while (input.IndexOf("href") != -1)
+        //{
+        //    input = input.Remove(0, input.IndexOf("href") + "href=".Length + 1);
+        //    url = input.Substring(0, input.IndexOf(">") - 1);
+        //    input = input.Remove(0, url.Length + 2);
+        //    text = input.Substring(0, input.IndexOf("</a>"));
+        //    oldStr = "<a href=\"" + url + "\">" + text + "</а>";
+        //    newStr = "[" + text + "](" + url + ")";
+        //    index = sb.ToString().IndexOf("<a");
+        //    sb = sb.Remove(index, oldStr.Length);
+        //    sb = sb.Insert(index, newStr);
+        //}
+        //Console.WriteLine(sb);
+
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string parsedHTML = Regex.Replace(text, "(<a href=\")(.*?)(\">)(.*?)(</a>)", "[$4]($2)");
+            Console.WriteLine(parsedHTML);
         }
     }
 }
