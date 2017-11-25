bba9aba106265f706ccbaecd92eb0f55e1c41883
Irina Hristova <hristova119@gmail.com>
2017-11-25 18:48:48 +0200

    Solved problem.

--- Strings/08. ExtractSentences/ExtractSentences.cs
+++ Strings/08. ExtractSentences/ExtractSentences.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Text;
     using System.Text.RegularExpressions;
 
     class ExtractSentences
@@ -10,28 +11,49 @@
         static void Main()
         {
             string searched = Console.ReadLine();
-            string sentences = Console.ReadLine();
+            string text = Console.ReadLine();
 
-            string wordPattern = @"(\sin\s)";
-            string sentencePattern = @"(\S.+?[.!?])(?=\s+|$)";
+            StringBuilder sb = new StringBuilder();
+            List<string> sentences = text.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries).ToList<string>();
+            char[] sep = text.Where(l => !char.IsLetter(l)).Distinct().ToArray();
 
-            Regex sentenceRG = new Regex(sentencePattern);
-            Regex wordRG = new Regex(wordPattern);
+            List<string> words = new List<string>();
 
-            MatchCollection matched = sentenceRG.Matches(sentences);
-
-            List<string> res = new List<string>();
-
-            foreach (var item in matched)
+            foreach (var sentence in sentences)
             {
-                if (wordRG.IsMatch(item.ToString()))
+                words = sentence.Split(sep, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                if (words.Contains(searched))
                 {
-                    res.Add(item.ToString());
+                    sb.Append(sentence.Trim());
+                    sb.Append(". ");
                 }
             }
+            Console.WriteLine(sb.ToString().Trim());
 
-            Console.WriteLine(string.Join(" ", res));
+            //string searched = Console.ReadLine();
+            //string sentences = Console.ReadLine();
+
+            //string wordPattern = @"(\sin\s)";
+            //string sentencePattern = @"(\S.+?[.!?])(?=\s+|$)";
+
+            //Regex sentenceRG = new Regex(sentencePattern);
+            //Regex wordRG = new Regex(wordPattern);
+
+            //MatchCollection matched = sentenceRG.Matches(sentences);
+
+            //List<string> res = new List<string>();
+
+            //foreach (var item in matched)
+            //{
+            //    if (wordRG.IsMatch(item.ToString()))
+            //    {
+            //        res.Add(item.ToString());
+            //    }
+            //}
+
+            //Console.WriteLine(string.Join(" ", res));
 
         }
-    }    
+    }
 }
