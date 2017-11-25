7176c873733bf35ddbbf93f7c1e49cc8242806f2
Irina Hristova <hristova119@gmail.com>
2017-11-25 11:12:42 +0200

    Solving problem.

--- Strings/08. ExtractSentences/ExtractSentences.cs
+++ Strings/08. ExtractSentences/ExtractSentences.cs
@@ -1,11 +1,23 @@
 ﻿namespace _08.ExtractSentences
 {
     using System;
+    using System.Text.RegularExpressions;
 
     class ExtractSentences
     {
         static void Main()
         {
+            //reading the input
+            string input = Console.ReadLine();
+
+            //constructing the pattern to be matched
+            Regex rg = new Regex(@"(\S.+?[.!?])(?=\s+|$)");
+
+            //iterating through the matches and printing them
+            foreach (Match match in rg.Matches(input))
+            {
+                Console.WriteLine(match.Value);
+            }
         }
     }
 }
