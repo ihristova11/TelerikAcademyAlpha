174d57b9d116e3470b40b7a5f4794653fa07bf9f
Irina Hristova <hristova119@gmail.com>
2017-11-25 15:12:22 +0200

    Solving problem.

--- Strings/08. ExtractSentences/ExtractSentences.cs
+++ Strings/08. ExtractSentences/ExtractSentences.cs
@@ -1,6 +1,7 @@
 ﻿namespace _08.ExtractSentences
 {
     using System;
+    using System.Linq;
     using System.Text.RegularExpressions;
 
     class ExtractSentences
@@ -8,20 +9,16 @@
         static void Main()
         {
             //reading the input
-            string searchedWord = Console.ReadLine();
-            string input = Console.ReadLine();
-
-            //constructing the pattern to be matched
-            Regex rg = new Regex(@"(\S.+?[.!?])(?=\s+|$)");
-            Regex wordMatch = new Regex(@"^(.*?(\bpass\b)[^$]*)$");
-            //iterating through the matches and printing them
-            foreach (Match match in rg.Matches(input))
+            //string word = Console.ReadLine();
+            //string input = Console.ReadLine();
+            string text = "some text in here intherein";
+            string pattern = @"\bin\s";
+            Regex rg = new Regex(pattern);
+            MatchCollection col = rg.Matches(text);
+            foreach (Match match in col)
             {
-                foreach (Match word in wordMatch.Matches(match.Value))
-                {
-                    Console.WriteLine(match.Value);
-                }
-                //Console.WriteLine(match.Value);
+
+                Console.WriteLine(match.Value);
             }
         }
     }
