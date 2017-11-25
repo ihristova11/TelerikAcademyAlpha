7c66b2971a995e6872b28daebe4a24863ff36f14
Irina Hristova <hristova119@gmail.com>
2017-11-25 16:01:04 +0200

    Solved problem.

--- Strings/08. ExtractSentences/ExtractSentences.cs
+++ Strings/08. ExtractSentences/ExtractSentences.cs
@@ -1,30 +1,36 @@
 ﻿namespace _08.ExtractSentences
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Text.RegularExpressions;
 
     class ExtractSentences
     {
         static void Main()
-        {            
+        {
             string searched = Console.ReadLine();
             string sentences = Console.ReadLine();
 
-            string wordPattern = @"(\bin\s)";
+            string wordPattern = @"(\sin\s)";
             string sentencePattern = @"(\S.+?[.!?])(?=\s+|$)";
+
             Regex sentenceRG = new Regex(sentencePattern);
             Regex wordRG = new Regex(wordPattern);
 
             MatchCollection matched = sentenceRG.Matches(sentences);
+
+            List<string> res = new List<string>();
+
             foreach (var item in matched)
             {
                 if (wordRG.IsMatch(item.ToString()))
                 {
-                    Console.WriteLine(item);
+                    res.Add(item.ToString());
                 }
             }
 
+            Console.WriteLine(string.Join(" ", res));
         }
-    }
+    }    
 }
