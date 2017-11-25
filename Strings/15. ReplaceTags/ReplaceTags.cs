ae136d9b85f65116810f5d0f998f7c84e3661c1a
Irina Hristova <hristova119@gmail.com>
2017-11-25 19:58:40 +0200

    Solving problem.

--- Strings/15. ReplaceTags/ReplaceTags.cs
+++ Strings/15. ReplaceTags/ReplaceTags.cs
@@ -1,11 +1,40 @@
 ﻿namespace _15.ReplaceTags
 {
     using System;
+    using System.Text;
 
     class ReplaceTags
     {
         static void Main()
         {
+            string input = Console.ReadLine();
+            StringBuilder sb = new StringBuilder(input);
+            string copy = String.Copy(input);
+            // string url = input.Substring(input.IndexOf("href");
+            string url = "";
+            string text = "";
+            string oldStr;
+            string newStr;
+            while (input.IndexOf("href") != -1)
+            {
+                input = input.Remove(0, input.IndexOf("href") + "href=".Length + 1);
+                url = input.Substring(0, input.IndexOf(">") - 1);
+                input = input.Remove(0, url.Length + 2);
+                text = input.Substring(0, input.IndexOf("</a>"));
+                oldStr = "<a href=\"{0}\"" + url + "\">" + text + "</а>";
+                newStr = "[" + text + "](" + url + ")";
+                copy = copy.Replace(oldStr, newStr);
+                Console.WriteLine(copy);
+            }
+            Console.WriteLine();
+            Console.WriteLine(input);
+            Console.WriteLine();
+            Console.WriteLine(url);
+            Console.WriteLine();
+            Console.WriteLine(text);
+
+            Console.WriteLine("------------------");
+            Console.WriteLine(copy.ToString());
         }
     }
 }
