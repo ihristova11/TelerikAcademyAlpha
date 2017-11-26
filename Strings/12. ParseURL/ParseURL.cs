ed4decc3f976aa1baf087069cd7b760adef58e73
Irina Hristova <hristova119@gmail.com>
2017-11-26 19:22:25 +0200

    Made changes.

--- Strings/12. ParseURL/ParseURL.cs
+++ Strings/12. ParseURL/ParseURL.cs
@@ -8,11 +8,8 @@
         {
             string input = Console.ReadLine();
             string protocol = input.Substring(0, input.IndexOf(':'));
-
             input = input.Remove(0, input.IndexOf(':') + 3);
-
             string server = input.Substring(0, input.IndexOf('/'));
-
             input = input.Remove(0, input.IndexOf('/'));
             string resource = input;
 
