723d6a64234733bad7d332d0d73c70ce3d9eae39
Irina Hristova <hristova119@gmail.com>
2017-11-26 22:42:06 +0200

    Solved problem.

--- MultidimensionalArrays/03. SequenceInMatrix/SequenceInMatrix.cs
+++ MultidimensionalArrays/03. SequenceInMatrix/SequenceInMatrix.cs
@@ -5,169 +5,155 @@
 
     class SequenceInMatrix
     {
-        public static int rows;
-        public static int cols;
-        public static int[][] array;
-        public static int maxLen = 0;
-        public static int currLen = 1;
+        //public static int rows;
+        //public static int cols;
+        //public static int[][] array;
+        //public static int maxLen = 0;
+        //public static int currLen = 1;
 
         static void Main()
         {
-            //reading the input
-            string line = Console.ReadLine();
-            int[] input = line.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-            rows = input[0];
-            cols = input[1];
+            // reading the input
+            string sizes = Console.ReadLine();
+            string[] rowsAndCols = sizes.Trim(' ').Split(' ');
 
-            array = new int[rows][];
+            //saving the sizes 
+            int inputRows = int.Parse(rowsAndCols[0]);
+            int inputCols = int.Parse(rowsAndCols[1]);
+            string[][] matrix = new string[inputRows][];
 
-            for (int i = 0; i < rows; i++)
+            for (int i = 0; i < inputRows; i++)
             {
-                array[i] = new int[cols];
-                array[i] = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                matrix[i] = Console.ReadLine().Trim(' ').Split(' ');
             }
-            CalculateLine();
-            CalculateColumn();
-            //CalculateDiagonalLeft();
-            CalculateDiagonalRight();
 
+            // vars
+            int maxLen = 0;
+            int currLen = 1;
+
+            // rows check
+            for (int row = 0; row < inputRows; row++)
+            {
+                // reset for each row
+                currLen = 1;
+
+                for (int col = 1; col < inputCols; col++)
+                {
+                    if (matrix[row][col] == matrix[row][col - 1])
+                    {
+                        currLen++;
+                    }
+                    else
+                    {
+                        maxLen = maxLen < currLen ? currLen : maxLen;
+                        currLen = 1;
+                    }
+                }
+
+                // double check for last element on row
+                maxLen = maxLen < currLen ? currLen : maxLen;
+            }
+
+            // cols check
+            for (int col = 0; col < inputCols; col++)
+            {
+                // reset for each col
+                currLen = 1;
+
+                for (int row = 1; row < inputRows; row++)
+                {
+                    if (matrix[row][col] == matrix[row - 1][col])
+                    {
+                        currLen++;
+                    }
+                    else
+                    {
+                        // check if currLen is longest
+                        maxLen = maxLen < currLen ? currLen : maxLen;
+                        //reset currLen
+                        currLen = 1;
+                    }
+                }
+
+                // double check currLen for last element
+                maxLen = maxLen < currLen ? currLen : maxLen;
+            }
+
+            //check diagonals (left diagonal)
+            for (int Col = 1; Col < inputCols; Col++)
+            {
+                currLen = 1;
+
+                for (int curMod = 1; curMod <= Math.Min(Col, inputRows - 1); curMod++)
+                {
+                    if (matrix[0 + curMod][Col - curMod] ==
+                        matrix[0 + (curMod - 1)][Col - (curMod - 1)])
+                    {
+                        currLen++;
+                    }
+                }
+
+                maxLen = maxLen < currLen ? currLen : maxLen;
+            }
+
+            for (int Row = 1; Row < inputRows; Row++)
+            {
+                currLen = 1;
+
+                for (int curMod = 1; curMod <= Math.Min(inputRows - 1 - Row - 1, inputCols - 2); curMod++)
+                {
+                    if (matrix[Row + curMod][(inputCols - 1) - curMod] ==
+                        matrix[Row + (curMod + 1)][(inputCols - 1) - (curMod + 1)])
+                    {
+                        currLen++;
+                    }
+                }
+
+                maxLen = maxLen < currLen ? currLen : maxLen;
+            }
+
+            // right diagonal check
+            for (int Row = inputRows - 2; Row >= 0; Row--)
+            {
+                currLen = 1;
+
+                for (int curMod = 1;
+                    curMod <= Math.Min(inputRows - Row - 1, inputCols - 1);
+                    curMod++)
+                {
+                    if (matrix[Row + curMod][0 + curMod] ==
+                        matrix[Row + (curMod - 1)][0 + (curMod - 1)])
+                    {
+                        currLen++;
+                    }
+                }
+
+                maxLen = maxLen < currLen ? currLen : maxLen;
+            }
+
+            // Top Right
+            for (int Row = 1;
+                     Row < inputRows - 1;
+                     Row++)
+            {
+                currLen = 1;
+
+                for (int curMod = 1;
+                    curMod <= Math.Min(Row - 1, inputCols - 1);
+                    curMod++)
+                {
+                    if (matrix[Row - curMod][(inputCols - 1) - curMod] ==
+                        matrix[Row - (curMod - 1)][(inputCols - 1) - (curMod - 1)])
+                    {
+                        currLen++;
+                    }
+                }
+
+                maxLen = maxLen < currLen ? currLen : maxLen;
+            }
+
+            // print maxLen
             Console.WriteLine(maxLen);
-
         }
-        //working
-        public static void CalculateLine()
-        {
-            for (int i = 0; i < rows; i++)
-            {
-                currLen = 1;
-
-                for (int j = 1; j < cols; j++)
-                {
-                    if (array[i][j - 1] == array[i][j])
-                    {
-                        currLen++;
-                    }
-                    else
-                    {
-                        currLen = 1;
-                    }
-                    maxLen = maxLen < currLen ? currLen : maxLen;
-                }
-            }
-        }
-        //working
-        public static void CalculateColumn()
-        {
-            for (int i = 0; i < cols; i++)
-            {
-                currLen = 1;
-                for (int j = 1; j < rows; j++)
-                {
-                    if (array[j - 1][i] == array[j][i])
-                    {
-                        currLen++;
-                    }
-                    else
-                    {
-                        currLen = 1;
-                    }
-                    maxLen = maxLen < currLen ? currLen : maxLen;
-                }
-            }
-        }
-
-        public static void CalculateDiagonalRight()
-        {
-            for (int i = 0; i < rows * 2 - 1; i++)
-            {
-                currLen = 1;
-                int row;
-                int col;
-                if (i < rows)
-                {
-                    row = rows - i - 1;
-                    col = 0;
-
-                    for (int j = 0; j < i; j++)
-                    {
-                        if (array[row][col] == array[row + 1][col + 1])
-                        {
-                            currLen++;
-                        }
-                        else
-                        {
-                            currLen = 1;
-                        }
-                        maxLen = maxLen < currLen ? currLen : maxLen;
-                        row++;
-                        col++;
-                    }
-                }
-                else
-                {
-                    row = 0;
-                    col = i - rows + 1;
-
-                    for (int j = 0; j < 2 * rows - i - 2; j++)
-                    {
-                        if (array[row][col] == array[row + 1][col + 1])
-                        {
-                            currLen++;
-                        }
-                        else
-                        {
-                            currLen = 1;
-                        }
-                        maxLen = maxLen < currLen ? currLen : maxLen;
-                        row++;
-                        col++;
-                    }
-                }
-
-            }
-
-            //public static void CalculateDiagonalLeft()
-            //{
-            //    for (int i = rows - 1, j = cols - 1; j >= 0;)
-            //    {
-            //        for (int startR = i, startC = j; ;)
-            //        {
-            //            if (((startR + 1) < rows) && ((startC + 1) < cols))
-            //            {
-            //                startR++;
-            //                startC++;
-
-            //                if (array[startC][startR] == array[startC - 1][startR - 1])
-            //                {
-            //                    currLen++;
-            //                }
-            //                else
-            //                {
-            //                    maxLen = maxLen < currLen ? currLen : maxLen;
-            //                    currLen = 0;
-            //                }
-            //            }
-            //            else
-            //            {
-            //                break;
-            //            }
-            //        }
-
-            //        if (i != 0)
-            //        {
-            //            i--;
-            //        }
-            //        else
-            //        {
-            //            j++;
-            //        }
-
-            //        maxLen = maxLen < currLen ? currLen : maxLen;
-            //        currLen = 0;
-            //    }
-
-
-        }
-
     }
 }
