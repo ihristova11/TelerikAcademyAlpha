4c06f6ebd33476e00487dca38c1d5bb33be9a897
Irina Hristova <hristova119@gmail.com>
2017-11-26 13:49:12 +0200

    Added new way to solve the problem.

--- Strings/23. SeriesOfLetters/SeriesOfLetters.cs
+++ Strings/23. SeriesOfLetters/SeriesOfLetters.cs
@@ -1,26 +1,38 @@
 ﻿namespace _23.SeriesOfLetters
 {
     using System;
+    using System.Collections.Generic;
     using System.Text;
 
     class SeriesOfLetters
     {
         static void Main()
         {
-            StringBuilder sb = new StringBuilder(Console.ReadLine());
-            int i = 0;
-            while(i != sb.Length - 1)
+            //solution with HashSet
+            string input = Console.ReadLine();
+            HashSet<string> hs = new HashSet<string>();
+
+            foreach (var ch in input)
             {
-                if (sb[i] == sb[i + 1])
-                {
-                    sb.Remove(i, 1);
-                }
-                else
-                {
-                    i++;
-                }
+                hs.Add(ch.ToString());
             }
-            Console.WriteLine(sb.ToString());
+            Console.WriteLine(string.Join("", hs));
+
+            //solution with StringBuilder
+            //StringBuilder sb = new StringBuilder(Console.ReadLine());
+            //int i = 0;
+            //while(i != sb.Length - 1)
+            //{
+            //    if (sb[i] == sb[i + 1])
+            //    {
+            //        sb.Remove(i, 1);
+            //    }
+            //    else
+            //    {
+            //        i++;
+            //    }
+            //}
+            //Console.WriteLine(sb.ToString());
         }
     }
 }
