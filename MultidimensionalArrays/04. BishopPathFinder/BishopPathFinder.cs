6031dc60e0a37cdab1597a6bad082a6d29387112
Irina Hristova <hristova119@gmail.com>
2017-11-27 00:25:15 +0200

    Solving problem.

--- MultidimensionalArrays/04. BishopPathFinder/BishopPathFinder.cs
+++ MultidimensionalArrays/04. BishopPathFinder/BishopPathFinder.cs
@@ -9,15 +9,50 @@
         public static int[,] matrix;
         public static int row;
         public static int col;
-
+        public static int rows;
+        public static int cols;
         static void Main()
         {
-
+            FillTheMatrix();
+            Print();
         }
 
         public static void FillTheMatrix()
         {
-            int[] input = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] input = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();            
+            rows = input[0];
+            cols = input[1];
+            matrix = new int[rows, cols];
+            matrix[rows - 1, 0] = 0;
+            int num = 3;
+            for (int i = rows - 2; i >= 0; i--)
+            {
+                for (int index = 0; index <= Math.Min(rows - 1 - i, cols - 1); index++)
+                {
+                    matrix[row + index, 0 + index] = num * index;
+                }
+            }
+
+            for (int Row = 1; Row < rows - 1; Row++)
+            {
+
+                for (int curMod = 1; curMod <= Math.Min(Row - 1, cols - 1); curMod++)
+                {
+                    matrix[Row - curMod, (cols - 1) - curMod] = num * curMod;
+                }
+            }
+        }
+
+        public static void Print()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
