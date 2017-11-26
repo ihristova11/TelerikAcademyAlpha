e875dd61de15890b88fae09a2f11ef85c019da45
Irina Hristova <hristova119@gmail.com>
2017-11-26 18:56:22 +0200

    Solved problem.

--- Strings/06. StringLength/StringLength.cs
+++ Strings/06. StringLength/StringLength.cs
@@ -7,11 +7,9 @@
         static void Main()
         {
             string input = Console.ReadLine();
-            int len = input.Length;
 
             if (input.Length < 20)
             {
-                //Console.WriteLine(input.PadRight((20 - len), '*'));
                 input = input.PadRight(20, '*');
             }
 
