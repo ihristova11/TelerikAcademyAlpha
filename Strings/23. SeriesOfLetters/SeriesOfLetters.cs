ad0da48f3617fbdd78a284db11d91b6456df806a
Irina Hristova <hristova119@gmail.com>
2017-11-26 13:50:49 +0200

    Solved problem.

--- Strings/23. SeriesOfLetters/SeriesOfLetters.cs
+++ Strings/23. SeriesOfLetters/SeriesOfLetters.cs
@@ -9,30 +9,30 @@
         static void Main()
         {
             //solution with HashSet
-            string input = Console.ReadLine();
-            HashSet<string> hs = new HashSet<string>();
+            //string input = Console.ReadLine();
+            //HashSet<string> hs = new HashSet<string>();
 
-            foreach (var ch in input)
-            {
-                hs.Add(ch.ToString());
-            }
-            Console.WriteLine(string.Join("", hs));
+            //foreach (var ch in input)
+            //{
+            //    hs.Add(ch.ToString());
+            //}
+            //Console.WriteLine(string.Join("", hs));
 
             //solution with StringBuilder
-            //StringBuilder sb = new StringBuilder(Console.ReadLine());
-            //int i = 0;
-            //while(i != sb.Length - 1)
-            //{
-            //    if (sb[i] == sb[i + 1])
-            //    {
-            //        sb.Remove(i, 1);
-            //    }
-            //    else
-            //    {
-            //        i++;
-            //    }
-            //}
-            //Console.WriteLine(sb.ToString());
+            StringBuilder sb = new StringBuilder(Console.ReadLine());
+            int i = 0;
+            while (i != sb.Length - 1)
+            {
+                if (sb[i] == sb[i + 1])
+                {
+                    sb.Remove(i, 1);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            Console.WriteLine(sb.ToString());
         }
     }
 }
