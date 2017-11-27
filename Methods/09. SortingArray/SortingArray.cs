4a46707d943da3f67ebfd636ca5f8419014cf782
Irina Hristova <hristova119@gmail.com>
2017-11-27 22:53:38 +0200

    Solved last problem.

---
+++ Methods/09. SortingArray/SortingArray.cs
@@ -0,0 +1,35 @@
+﻿namespace _09.SortingArray
+{
+    using System;
+    using System.Linq;
+
+    class SortingArray
+    {
+        static void Main()
+        {
+            int len = int.Parse(Console.ReadLine());
+            int[] numbers = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Sort(numbers);
+        }
+
+        public static void Sort(int[] numbers)
+        {
+            int temp = 0;
+
+            for (int write = 0; write < numbers.Length; write++)
+            {
+                for (int sort = 0; sort < numbers.Length - 1; sort++)
+                {
+                    if (numbers[sort] > numbers[sort + 1])
+                    {
+                        temp = numbers[sort + 1];
+                        numbers[sort + 1] = numbers[sort];
+                        numbers[sort] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
