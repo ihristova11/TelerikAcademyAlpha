d445414136f95edecb2bb892afb55d3a817e41e0
Irina Hristova <hristova119@gmail.com>
2017-11-27 15:07:17 +0200

    Solved problem.

--- Methods/02. GetLargestNumber/Program.cs
+++ Methods/02. GetLargestNumber/Program.cs
@@ -6,6 +6,18 @@
     {
         static void Main()
         {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int maxNumber = GetMax(GetMax(firstNum, secondNum), GetMax(secondNum, thirdNum));
+            Console.WriteLine(maxNumber);
+        }
+
+        public static int GetMax(int firstNumber, int secondNumber)
+        {
+            int maxNumber = firstNumber < secondNumber ? secondNumber : firstNumber;
+
+            return maxNumber;
         }
     }
 }
