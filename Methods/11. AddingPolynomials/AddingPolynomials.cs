b6fc9aac78494b65ac40f1b8768b7edc6b119d2b
Irina Hristova <hristova119@gmail.com>
2017-11-27 16:54:15 +0200

    Solved problem.

---
+++ Methods/11. AddingPolynomials/AddingPolynomials.cs
@@ -0,0 +1,27 @@
+﻿namespace _11.AddingPolynomials
+{
+    using System;
+    using System.Linq;
+
+    class AddingPolynomials
+    {
+        static void Main()
+        {
+            int len = int.Parse(Console.ReadLine());
+            int[] firstExpCoeff = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] secondExpCoeff = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Console.WriteLine(AddPolynomials(len, firstExpCoeff, secondExpCoeff));
+        }
+
+        public static string AddPolynomials(int len, int[] firstExpCoeff, int[] secondExpCoeff)
+        {
+            var result = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                result[i] += firstExpCoeff[i] + secondExpCoeff[i];
+            }
+
+            return string.Join(" ", result);
+        }
+    }
+}
