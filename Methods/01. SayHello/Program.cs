46760dafda40bf319931488ef1fd539b912caeca
Irina Hristova <hristova119@gmail.com>
2017-11-27 12:34:18 +0200

    Solved problem.

---
+++ Methods/01. SayHello/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _01.SayHello
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+
+            Console.WriteLine(SayHello(name));
+        }
+
+        public static string SayHello(string name)
+        {
+            return "Hello, " + name + "!";
+        }
+    }
+}
