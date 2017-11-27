66a039ece20bae877a92cd99e34eaa193a841fe2
Irina Hristova <hristova119@gmail.com>
2017-11-27 15:41:07 +0200

    Solved problems.

---
+++ Methods/04. AppearanceCount/AppearanceCount.cs
@@ -0,0 +1,27 @@
+﻿namespace _04.AppearanceCount
+{
+    using System;
+    using System.Linq;
+
+    class AppearanceCount
+    {
+        static void Main()
+        {
+            Console.WriteLine(CountAppearances());
+        }
+
+        public static int CountAppearances()
+        {
+            int times = 0;
+            int len = int.Parse(Console.ReadLine());
+            string inputArr = Console.ReadLine();
+            int searchedNum = int.Parse(Console.ReadLine());
+            int[] numbers = inputArr.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray().ToArray();
+            foreach (var number in numbers)
+            {
+                if (number == searchedNum) times++;
+            }
+            return times;
+        }
+    }
+}
---
+++ Methods/05. LargerThanNeighbours/LargerThanNeightbours.cs
@@ -0,0 +1,32 @@
+﻿namespace _05.LargerThanNeighbours
+{
+    using System;
+    using System.Linq;
+
+    class LargerThanNeightbours
+    {
+        static void Main()
+        {
+            Console.WriteLine(CheckNumberIfLarger());
+        }
+
+        public static int CheckNumberIfLarger()
+        {
+            int largerNumbers = 0;
+            int len = int.Parse(Console.ReadLine());
+            string line = Console.ReadLine();
+            int[] numbers = line.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            for (int i = 1; i < len - 1; i++)
+            {
+                //largerNumbers = (numbers[i - 1] < numbers[i] && numbers[i] > numbers[i + 1]) ? largerNumbers++ : largerNumbers;
+                if(numbers[i - 1] < numbers[i] && numbers[i] > numbers[i + 1])
+                {
+                    largerNumbers++;
+                }
+            }
+
+            return largerNumbers;
+        }
+    }
+}
