5751e9bdb0c42a5f720fa3876608f1c0f2144521
Irina Hristova <hristova119@gmail.com>
2017-11-27 16:01:17 +0200

    Solved problem.

---
+++ Methods/07. ReverseNumber/ReverseNumber.cs
@@ -0,0 +1,28 @@
+﻿namespace _07.ReverseNumber
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    class ReverseNumber
+    {
+        static void Main()
+        {
+            decimal number = decimal.Parse(Console.ReadLine());
+            Console.WriteLine(ReverseDecimal(number.ToString()));
+        }
+
+        public static string ReverseDecimal(string number)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                sb.Append(number[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
