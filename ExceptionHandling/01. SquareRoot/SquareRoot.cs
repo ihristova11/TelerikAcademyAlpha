1cdebccb821e7c43c17374323959c98f39eb1519
Irina Hristova <hristova119@gmail.com>
2017-11-28 08:45:43 +0200

    Solved problems.

---
+++ ExceptionHandling/01. SquareRoot/SquareRoot.cs
@@ -0,0 +1,31 @@
+﻿namespace _01.SquareRoot
+{
+    using System;
+
+    class SquareRoot
+    {
+        static void Main()
+        {
+            try
+            {
+                double number = double.Parse(Console.ReadLine());
+                if (number > 0)
+                {
+                    Console.WriteLine(Math.Sqrt(number).ToString("F3"));
+                }
+                else
+                {
+                    throw new FormatException();
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
---
+++ ExceptionHandling/02. EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,52 @@
+﻿namespace _02.EnterNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class EnterNumbers
+    {
+        public static int number;
+        public static int count = 1;
+        public static List<int> enteredNumbers = new List<int>();
+
+        static void Main()
+        {
+            ReadNumber(1, 100);
+        }
+
+        public static void ReadNumber(int start, int end)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                enteredNumbers.Add(int.Parse(Console.ReadLine()));
+            }
+
+            try
+            {
+                if (enteredNumbers.Any(x => x < start) || enteredNumbers.Any(x => x > end) || !IsSorted())
+                {
+                    throw new Exception();
+                }
+                Console.WriteLine(string.Join(" < ", enteredNumbers));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception");
+            }
+        }
+
+        public static bool IsSorted()
+        {
+            for (int i = 0; i < enteredNumbers.Count - 1; i++)
+            {
+                if (enteredNumbers[i] > enteredNumbers[i + 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
