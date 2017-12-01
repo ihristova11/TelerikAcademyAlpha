cd8de3974e24023f14d1f7f31a041d12d5db2f4e
Irina Hristova <hristova119@gmail.com>
2017-12-01 11:06:06 +0200

    Solving problem.

--- ExamTraining/01. Kitty/Kitty.cs
+++ ExamTraining/01. Kitty/Kitty.cs
@@ -2,17 +2,62 @@
 {
     using System;
     using System.Linq;
+    using System.Text;
 
     class Kitty
     {
         static void Main()
         {
             // reading the path for the kitty
-            string path = Console.ReadLine();
-            int[] instructions = Console.ReadLine()
+            StringBuilder path = Console.ReadLine();
+            int[] directions = Console.ReadLine()
                 .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
                 .ToArray();
+
+            int position = 0;
+            int souls = 0;
+            int food = 0;
+            int deadlocks = 0;
+            for (int direction = 0; direction < directions.Length; direction++)
+            {
+                switch(path[direction])
+                {
+                    case '@':
+                        souls++;
+                        path[direction] = 'v';
+                        break;
+                    case 'x': break;
+                        if (position % 2 == 0)
+                        {
+                            if(souls > 0)
+                            {
+                                souls--;
+                                path[position] = '@';
+                            }
+                            else
+                            {
+                                Console.WriteLine("You are deadlocked, you greedy kitty!");
+                                Console.WriteLine("Jumps before deadlock: {0}", direction);
+                            }
+                        }
+                        else
+                        {
+                            if(food > 0 )
+                            {
+                                food--;
+                                path[position] = '*';
+                            }
+                            else
+                            {
+                                Console.WriteLine("You are deadlocked, you greedy kitty!");
+                                Console.WriteLine("Jumps before deadlock: {0}", direction);
+                            }
+                        }
+                    case '*': break;
+                }
+                position = directions[direction];
+            }
 
         }
     }
