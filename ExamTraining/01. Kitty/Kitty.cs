c5fd4521dcb6636e36b0aa1e429254a4b4667438
Irina Hristova <hristova119@gmail.com>
2017-12-01 10:57:35 +0200

    Solving problem.

--- ExamTraining/01. Kitty/Kitty.cs
+++ ExamTraining/01. Kitty/Kitty.cs
@@ -1,11 +1,19 @@
 ﻿namespace _01.Kitty
 {
     using System;
+    using System.Linq;
 
     class Kitty
     {
         static void Main()
         {
+            // reading the path for the kitty
+            string path = Console.ReadLine();
+            int[] instructions = Console.ReadLine()
+                .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
         }
     }
 }
