1d3a4a87967dd79445ac19a5196a94028732c232
Irina Hristova <hristova119@gmail.com>
2017-12-01 11:56:46 +0200

    Solving problem.

--- TrainingExam/02. BitShiftMatrix/BitShiftMatrix.cs
+++ TrainingExam/02. BitShiftMatrix/BitShiftMatrix.cs
@@ -5,14 +5,44 @@
 
     class BitShiftMatrix
     {
+        public static int r;
+        public static int c;
+        public static int[,] matrix;
         static void Main()
         {
             int[] input = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
                 .ToArray();
 
-            int r = input[0];
-            int c = input[1];
+            r = input[0];
+            c = input[1];
+            matrix = new int[r, c];
+            //int numberOfMoves = int.Parse(Console.ReadLine());
+            FillTheMatrix();
+            PrintTheMatrix();
+        }
+
+        public static void FillTheMatrix()
+        {
+            for (int i = r - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    matrix[i, j] = (int)(Math.Pow(2, r - 1 - i) * Math.Pow(2, j));
+                }
+            }
+        }
+
+        public static void PrintTheMatrix()
+        {
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    Console.Write((matrix[i, j] + " ").PadLeft(4));
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
