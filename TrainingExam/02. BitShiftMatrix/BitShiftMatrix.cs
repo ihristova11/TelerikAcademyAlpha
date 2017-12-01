c9689ade165242639a1951fba01b8b96cc1ae8b0
Irina Hristova <hristova119@gmail.com>
2017-12-01 11:37:57 +0200

    Solving problems.

--- TrainingExam/02. BitShiftMatrix/BitShiftMatrix.cs
+++ TrainingExam/02. BitShiftMatrix/BitShiftMatrix.cs
@@ -1,11 +1,18 @@
 ﻿namespace _02.BitShiftMatrix
 {
     using System;
+    using System.Linq;
 
     class BitShiftMatrix
     {
         static void Main()
         {
+            int[] input = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int r = input[0];
+            int c = input[1];
         }
     }
 }
