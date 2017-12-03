7ae9efc3c86360bc7cad01393723501424d4a024
Irina Hristova <hristova119@gmail.com>
2017-12-03 18:11:49 +0200

    Solving problem.

--- AlphaExam1/02. Move/Move.cs
+++ AlphaExam1/02. Move/Move.cs
@@ -7,15 +7,22 @@
     {
         static void Main()
         {
+            //reading input
             int position = int.Parse(Console.ReadLine());
             int[] numbers = Console.ReadLine()
                 .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
                 .ToArray();
+            string direction = string.Empty;
+            int size;
+            int steps;
             bool notExit = true;
+            string line = string.Empty;
+
 
             while(notExit)
             {
+                line = Console.ReadLine();
 
             }
         }
