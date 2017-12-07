382242d85c036e38cf223a9df52c3319cc33ba3e
Irina Hristova <hristova119@gmail.com>
2017-12-07 22:39:20 +0200

    Completed MobilePhone class.

--- MODULE 1/C# OOP/DefiningClasses-Part1/MobilePhoneComponents/MobilePhone.cs
+++ MODULE 1/C# OOP/DefiningClasses-Part1/MobilePhoneComponents/MobilePhone.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class MobilePhone
     {
@@ -149,6 +150,43 @@
             {
                 return meizuMX4Pro;
             }
+        }
+
+        public List<Call> CallHistory
+        {
+            get
+            {
+                return new List<Call>(this.calls);
+            }
+        }
+
+        public void AddCalls(Call call)
+        {
+            this.calls.Add(call);
+        }
+
+        public void DeleteCall(Call call)
+        {
+            this.calls.Remove(call);
+        }
+
+        public void DeleteCallHistory()
+        {
+            this.calls.Clear();
+        }
+
+        public decimal GetTotalCallPrice(decimal pricePerMinute)
+        {
+            decimal allCallsInSecs = (decimal)(this.calls.Select(x => x.Duration).Sum());
+            return pricePerMinute * allCallsInSecs / 60.0m;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(@"{0} {1} {2} {3} {4}", this.Manufacturer, this.Model, this.OSSet ? string.Format("{1}\t(OS: {0})", this.OS, Environment.NewLine) : String.Empty,
+                 !this.batterySet ? String.Empty : String.Format("{0}{0}{1}", Environment.NewLine, this.Battery.ToString()),
+                 !this.displaySet ? String.Empty : String.Format("{0}{0}{1}", Environment.NewLine, this.Display.ToString()))
+                 .Trim();
         }
 
         public enum MobileManufacturer
