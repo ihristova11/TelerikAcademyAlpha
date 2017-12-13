59f4231b1f898704a0fd02c6db99fe3e231d59d0
Irina Hristova <hristova119@gmail.com>
2017-12-13 22:19:36 +0200

    Solving problem.

--- MODULE 1/C# OOP/DefiningClasses-Part2/Point3D/Point3D.cs
+++ MODULE 1/C# OOP/DefiningClasses-Part2/Point3D/Point3D.cs
@@ -4,23 +4,21 @@
 
     public struct Point3D
     {
-        private double x;
-        private double y;
-        private double z;
+       // private double x, y, z;
         private static readonly Point3D O = new Point3D(0, 0, 0);
 
         public Point3D(double x, double y, double z)
         {
-            this.x = x;
-            this.y = y;
-            this.z = z;
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
         }
 
-        public double X { get { return this.x; } }
+        public double X { get; set; }
 
-        public double Y { get { return this.y; } }
+        public double Y { get;  set; }
 
-        public double Z { get { return this.z; } }
+        public double Z { get; set; }
 
         public static Point3D PointO
         {
@@ -32,7 +30,7 @@
 
         public override string ToString()
         {
-            return string.Format("{0} {1} {2}", this.x, this.y, this.z);
+            return string.Format("{0}, {1}, {2}", this.X, this.Y, this.Z);
         }
 
     }
