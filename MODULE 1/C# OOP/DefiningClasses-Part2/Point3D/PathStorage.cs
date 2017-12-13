907c61d2cabe92515cd2845cbe027ccbba85319f
Irina Hristova <hristova119@gmail.com>
2017-12-13 22:37:46 +0200

    Solving problem.

--- MODULE 1/C# OOP/DefiningClasses-Part2/Point3D/PathStorage.cs
+++ MODULE 1/C# OOP/DefiningClasses-Part2/Point3D/PathStorage.cs
@@ -1,9 +1,19 @@
 ﻿namespace Point3D
 {
     using System;
-
+    using IO = System.IO;
+    using System.Linq;
     public static class PathStorage
     {
+        public static void Save(Path path, string pathName)
+        {
+            string fullPath = IO.Path.Combine(@"..\Paths", String.Format("{0}.txt", pathName.Trim()));
 
+        }
+
+        public static void Load(string pathName)
+        {
+
+        }
     }
 }
