af1969c22797bbe743b25cda22dc7eb9ae39a387
Irina Hristova <hristova119@gmail.com>
2017-12-13 22:58:06 +0200

    Added new task.

---
+++ MODULE 1/C# OOP/DefiningClasses-Part2/GenericClass/GenericList.cs
@@ -0,0 +1,37 @@
+﻿namespace GenericClass
+{
+    public class GenericList<T>
+    {
+        private T[] list;
+        private int lastIndex;
+
+        public GenericList(int capacity)
+        {
+            this.list = new T[capacity];
+            this.lastIndex = -1;
+        }
+
+        public void Add(T element)
+        {
+            if(lastIndex + 1 == this.list.Length)
+            {
+                DoubleSize();
+            }
+
+            this.list[++lastIndex] = element;
+        }
+
+        private void DoubleSize()
+        {
+            int newSize = this.list.Length == 0 ? 2 : this.list.Length * 2;
+
+            T[] newList = new T[newSize];
+            for (int i = 0; i <= this.lastIndex; i++)
+            {
+                newList[i] = this.list[i];
+            }
+
+            this.list = newList;
+        }
+    }
+}
