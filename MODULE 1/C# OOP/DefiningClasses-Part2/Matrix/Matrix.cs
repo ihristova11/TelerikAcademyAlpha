57096430823dc7264ef0c47923c57ed125756857
Irina Hristova <hristova119@gmail.com>
2017-12-14 00:03:54 +0200

    Solving problem.

--- MODULE 1/C# OOP/DefiningClasses-Part2/Matrix/Matrix.cs
+++ MODULE 1/C# OOP/DefiningClasses-Part2/Matrix/Matrix.cs
@@ -11,8 +11,8 @@ namespace Matrix
 
         public Matrix(int rowsCount, int colsCount)
         {
-            this.rows = rowsCount;
-            this.cols = colsCount;
+            this.Rows = rowsCount;
+            this.Cols = colsCount;
             this.matrix = new T[rowsCount, colsCount];
         }
 
@@ -20,17 +20,96 @@ namespace Matrix
         {
             get
             {
+                if ((row < 0 || row >= this.Rows) ||
+                    (col < 0 || col >= this.Cols))
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
                 return this.matrix[row, col];
             }
             set
             {
+                if ((row < 0 || row >= this.Rows) ||
+                    (col < 0 || col >= this.Cols))
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                
                 this.matrix[row, col] = value;
+            }
+        }
+
+        public int Rows
+        {
+            get
+            {
+                return this.rows;
+            }
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentOutOfRangeException("rows count", "The matrix must have at least one row.");
+                }
+                this.rows = value;
+            }
+        }
+
+        public int Cols
+        {
+            get
+            {
+                return this.cols;
+            }
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentOutOfRangeException("columns count", "The matrix must have at least one column.");
+                }
+                this.cols = value;
             }
         }
 
         public static Matrix<T> operator +(Matrix<T> firstMatrix, Matrix<T> secondMatrix)
         {
-            T[,] resultMatrix = new T[]
+            if (firstMatrix.Cols != secondMatrix.Cols || firstMatrix.Rows != secondMatrix.Rows)
+            {
+                throw new Exception("Addition cannot be applied to matrices with different dimensions.");
+            }
+
+            Matrix<T> result = new Matrix<T>(firstMatrix.Rows, firstMatrix.Cols);
+
+            for (int i = 0; i < firstMatrix.Rows; i++)
+            {
+                for (int j = 0; j < firstMatrix.Cols; j++)
+                {
+                    result[i, j] = (dynamic)firstMatrix[i, j] + secondMatrix[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix<T> operator -(Matrix<T> firstMatrix, Matrix<T> secondMatrix)
+        {
+            if (firstMatrix.Cols != secondMatrix.Cols || firstMatrix.Rows != secondMatrix.Rows)
+            {
+                throw new Exception("Addition cannot be applied to matrices with different dimensions.");
+            }
+
+            Matrix<T> result = new Matrix<T>(firstMatrix.Rows, firstMatrix.Cols);
+
+            for (int i = 0; i < firstMatrix.Rows; i++)
+            {
+                for (int j = 0; j < firstMatrix.Cols; j++)
+                {
+                    result[i, j] = (dynamic)firstMatrix[i, j] - secondMatrix[i, j];
+                }
+            }
+
+            return result;
         }
     }
 }
