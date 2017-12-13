c59edd9425403aa62358bf6cbdfe1136403dfeb2
Irina Hristova <hristova119@gmail.com>
2017-12-14 00:43:45 +0200

    Completed task.

---
+++ MODULE 1/C# OOP/DefiningClasses-Part2/Attribute/Attribute.cs
@@ -0,0 +1,23 @@
+﻿namespace Attribute
+{
+    using System;
+    
+    [Version("2.11")]
+    public class Program
+    {
+        static void Main()
+        {
+            typeof(Program).GetCustomAttributes(true);
+        }
+    }
+    [AttributeUsage(AttributeTargets.Struct | AttributeTargets.Class | AttributeTargets.Enum | AttributeTargets.Interface | AttributeTargets.Method)]
+    public class VersionAttribute : System.Attribute
+    {
+        public string Name { get; private set; }
+
+        public VersionAttribute(string name)
+        {
+            System.Console.WriteLine(name);
+        }
+    }
+}
