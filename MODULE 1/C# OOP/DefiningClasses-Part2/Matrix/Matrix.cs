35c3830e4b01469d0f73fff112dc77719358eacd
Irina Hristova <hristova119@gmail.com>
2017-12-13 23:48:48 +0200

    Added new task.

---
+++ MODULE 1/C# OOP/DefiningClasses-Part2/Matrix/Matrix.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Matrix
+{
+    public class Matrix<T>
+        where T : IComparable
+    {
+        private T[,] matrix;
+        
+
+        public T this[int row, int col]
+        {
+            get
+            {
+                return this.matrix[row, col];
+            }
+            set
+            {
+                this.matrix[row, col] = value;
+            }
+        }
+    }
+}
