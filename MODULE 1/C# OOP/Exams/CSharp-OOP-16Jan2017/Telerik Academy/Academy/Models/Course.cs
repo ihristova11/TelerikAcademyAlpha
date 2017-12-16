2af49c42a5f8c6379a0b28ff262de9297e37542e
Irina Hristova <hristova119@gmail.com>
2017-12-16 12:14:08 +0200

    Added new exam

---
+++ MODULE 1/C# OOP/Exams/CSharp-OOP-16Jan2017/Telerik Academy/Academy/Models/Course.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Academy.Models
+{
+    using Academy.Models.Contracts;
+
+    public class Course : ICourse
+    {
+        private string name;
+        private int lecturesPerWeek;
+        private DateTime endingDate;
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (value.Length < 3 || value.Length > 45)
+                {
+                    throw new ArgumentException("The name of the course must be between 3 and 45 symbols!");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int LecturesPerWeek { get { return this.lecturesPerWeek; }
+
+            set
+            {
+                if (value < 1 || value > 7)
+                {
+                    throw new ArgumentException("The number of lectures per week must be between 1 and 7!");
+                }
+
+                this.lecturesPerWeek = value;
+            }
+        }
+        public DateTime StartingDate { get; set; }
+
+        public DateTime EndingDate
+        {
+            get
+            {
+                return this.StartingDate.AddDays(30);
+            }
+            set { this.endingDate = value; }
+        }
+
+        public IList<IStudent> OnsiteStudents { get; }
+        public IList<IStudent> OnlineStudents { get; }
+        public IList<ILecture> Lectures { get; }
+
+        public override string ToString()
+        {
+            string msg = "  * There are no lectures in this course!";
+            return $"* Course\n - Name: {this.Name}\n - Lectures per week: {this.LecturesPerWeek}\n - Starting date: {this.StartingDate}\n - Ending date: {this.EndingDate}\n - Lectures: " + msg;
+        }
+    }
+}
---
+++ MODULE 1/C# OOP/Exams/CSharp-OOP-16Jan2017/Telerik Academy/Academy/Models/Trainer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Academy.Models.Contracts;
+
+namespace Academy.Models
+{
+    public class Trainer : ITrainer
+    {
+        private string username;
+
+        public string Username {
+            get { return this.username; }
+            set
+            {
+                if (value.Length < 3 || value.Length > 16 || value == null)
+                {
+                    throw new ArgumentException("User's username should be between 3 and 16 symbols long!");
+                }
+
+                this.username = value;
+            }
+        }
+
+        public IList<string> Technologies { get; set; }
+
+        public override string ToString()
+        {
+            string technologies = string.Join("; ", this.Technologies);
+            return $"* Trainer:\n - Username: {this.Username}\n - Technologies: " + technologies;
+        }
+    }
+}
