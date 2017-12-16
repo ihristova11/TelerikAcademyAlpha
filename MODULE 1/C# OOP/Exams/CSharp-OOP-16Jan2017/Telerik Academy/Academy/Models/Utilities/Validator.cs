356d5e01dcfa0edac2fcc8305390a3d1828bf81e
Irina Hristova <hristova119@gmail.com>
2017-12-16 14:01:01 +0200

    Added validator with constants

--- MODULE 1/C# OOP/Exams/CSharp-OOP-16Jan2017/Telerik Academy/Academy/Models/Utilities/Validator.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Academy.Models.Utilities
-{
-    public class Validator
-    {
-    }
-}
---
+++ MODULE 1/C# OOP/Exams/CSharp-OOP-16Jan2017/Telerik Academy/Academy/Models/Utils/Validator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Academy.Models.Utilities
+{
+    public class Validator
+    {
+        public static void CorrectName(string name, int min, int max, string message)
+        {
+            if (string.IsNullOrEmpty(name) || name.Length < min || name.Length > max)
+            {
+                throw new ArgumentException(message);
+            }
+        }
+
+        public static int CorrectLecturePerWeek(string lecturePerWeek)
+        {
+            int res;
+            try
+            {
+                res = int.Parse(lecturePerWeek);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException(Constants.CoursecorrectLecturePerWeek);
+            };
+
+            if (res < 1 || res > 7)
+            {
+                throw new ArgumentException(Constants.CoursecorrectLecturePerWeek);
+            }
+
+            return res;
+        }
+
+        public static object ParsEnum(Type type, string text)
+        {
+            object result = null;
+
+            try
+            {
+                result = Enum.Parse(type, text);
+            }
+            catch (ArgumentException)
+            {
+                throw new ArgumentException(Constants.NotValidTrack); //0 always mean - unknown type
+            }
+
+            return result;
+        }
+    }
+}
