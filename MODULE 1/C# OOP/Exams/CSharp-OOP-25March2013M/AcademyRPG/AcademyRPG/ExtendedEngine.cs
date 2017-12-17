2a0348d647efea86c8c30ab416a1b1492ffdfc15
Irina Hristova <hristova119@gmail.com>
2017-12-17 22:54:15 +0200

    Added Knight

---
+++ MODULE 1/C# OOP/Exams/CSharp-OOP-25March2013M/AcademyRPG/AcademyRPG/ExtendedEngine.cs
@@ -0,0 +1,43 @@
+﻿namespace AcademyRPG
+{
+    public class ExtendedEngine : Engine
+    {
+        public override void ExecuteCreateObjectCommand(string[] commandWords)
+        {
+            switch (commandWords[1])
+            {
+                case "lumberjack":
+                    {
+                        string name = commandWords[2];
+                        Point position = Point.Parse(commandWords[3]);
+                        int owner = int.Parse(commandWords[4]);
+                        this.AddObject(new Lumberjack(name, position, owner));
+                        break;
+                    }
+                case "guard":
+                    {
+                        string name = commandWords[2];
+                        Point position = Point.Parse(commandWords[3]);
+                        int owner = int.Parse(commandWords[4]);
+                        this.AddObject(new Guard(name, position, owner));
+                        break;
+                    }
+                case "tree":
+                    {
+                        int size = int.Parse(commandWords[2]);
+                        Point position = Point.Parse(commandWords[3]);
+                        this.AddObject(new Tree(size, position));
+                        break;
+                    }
+                case "knight":
+                    {
+                        string name = commandWords[2];
+                        Point position = Point.Parse(commandWords[3]);
+                        int owner = int.Parse(commandWords[4]);
+                        this.AddObject(new Knight(name, position, owner));
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ MODULE 1/C# OOP/Exams/CSharp-OOP-25March2013M/AcademyRPG/AcademyRPG/Knight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace AcademyRPG
+{
+    public class Knight : Character, IFighter
+    {
+        public Knight(string name, Point position, int owner) : base(name, position, owner)
+        {
+            this.AttackPoints = 100;
+            this.DefensePoints = 100;
+            this.HitPoints = 100;
+        }
+
+        public int AttackPoints { get; }
+        public int DefensePoints { get; }
+        public int GetTargetIndex(List<WorldObject> availableTargets)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
