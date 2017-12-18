327970fa0708313ce4821307152005225aca3b4f
Irina Hristova <hristova119@gmail.com>
2017-12-18 16:39:12 +0200

    Added new homework solution

---
+++ MODULE 1/C# OOP/Extension-Methods-Delegates/StringBuilderSubstring/Problem1.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace StringBuilderSubstring
+{
+    public static class Problem1
+    {
+        static void Main()
+        {
+            
+        }
+
+        public static StringBuilder Substring(this StringBuilder sb, int index, int length)
+        {
+
+        }
+    }
+}
