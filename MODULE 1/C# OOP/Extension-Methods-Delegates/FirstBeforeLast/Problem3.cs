c311475b21cac12718f2fea42ff7a661f9c3cea9
Irina Hristova <hristova119@gmail.com>
2017-12-18 17:34:33 +0200

    Solved Problem3

---
+++ MODULE 1/C# OOP/Extension-Methods-Delegates/FirstBeforeLast/Problem3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+namespace FirstBeforeLast
+{
+    public class Problem3
+    {
+        static void Main()
+        {
+            Student[] classOfStudents = new Student[3];
+            classOfStudents[0] = new Student("Ivan", "Ivanov");
+            classOfStudents[1] = new Student("Irina", "Hristova");
+            classOfStudents[2] = new Student("Pesho", "Peshov");
+            FindStudents(classOfStudents);
+        }
+
+        public static void FindStudents(Student[] collection)
+        {
+            var result =
+                from student in collection
+                where student.FirstName.CompareTo(student.LastName) == -1
+                select student;
+
+            foreach (var student in result)
+            {
+                Console.WriteLine("{0} {1}", student.FirstName, student.LastName);
+            }
+        }
+    }
+}
