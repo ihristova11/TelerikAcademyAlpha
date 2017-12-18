cbf373a0aa8df8fe16d2451cec4690f2eb0317be
Irina Hristova <hristova119@gmail.com>
2017-12-18 16:43:09 +0200

    Solved first problem.

--- MODULE 1/C# OOP/Extension-Methods-Delegates/StringBuilderSubstring/Problem1.cs
+++ MODULE 1/C# OOP/Extension-Methods-Delegates/StringBuilderSubstring/Problem1.cs
@@ -1,17 +1,26 @@
-﻿using System.Text;
-
-namespace StringBuilderSubstring
+﻿namespace StringBuilderSubstring
 {
+    using System;
+    using System.Text;
+
     public static class Problem1
     {
         static void Main()
         {
-            
+            //testing
+            StringBuilder strb = new StringBuilder("Irina");
+            Console.WriteLine(strb.Substring(0, 2));
         }
 
         public static StringBuilder Substring(this StringBuilder sb, int index, int length)
         {
+            StringBuilder result = new StringBuilder();
+            for (int i = index; i < index + length; i++)
+            {
+                result.Append(sb[i]);
+            }
 
+            return result;
         }
     }
 }
