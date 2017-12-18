888a622c7dc9d8664a5a08468cffd7cb032e307e
Irina Hristova <hristova119@gmail.com>
2017-12-18 17:55:59 +0200

    Solved Problem6.

---
+++ MODULE 1/C# OOP/Extension-Methods-Delegates/DivisibleBy7And3/Problem6.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace DivisibleBy7And3
+{
+    class Problem6
+    {
+        static void Main()
+        {
+            var numbers = new int[3];
+            numbers[0] = 5;
+            numbers[1] = 7;
+            numbers[2] = 42;
+            FindDivisible(numbers);
+        }
+
+        public static void FindDivisible(int[] numbers)
+        {
+            var divisible = numbers.Where(x => x % 21 == 0);
+
+            foreach (var number in divisible)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
