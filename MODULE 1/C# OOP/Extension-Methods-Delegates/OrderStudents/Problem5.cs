d7441db5480a7a385ede775bd163a938cbb43023
Irina Hristova <hristova119@gmail.com>
2017-12-18 17:49:06 +0200

    Solved Problem6.

---
+++ MODULE 1/C# OOP/Extension-Methods-Delegates/OrderStudents/Problem5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using FirstBeforeLast;
+
+namespace OrderStudents
+{
+    using System.Linq;
+
+    public class Problem5
+    {
+        static void Main()
+        {
+            var students = new List<Student>();
+            students.Add(new Student("Irina", "Hristova"));
+            students.Add(new Student("Ivan", "Ivanov"));
+            students.Add(new Student("Pesho", "Peshov"));
+            FindStudents(students);
+        }
+
+        public static void FindStudents(List<Student> students)
+        {
+            var found = students
+                .OrderByDescending(x => x.FirstName)
+                .ThenByDescending(x => x.LastName);
+
+            foreach (var student in found)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName}");
+            }
+        }
+    }
+}
