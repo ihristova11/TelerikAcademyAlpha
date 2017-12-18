6e099a7be3480a68d6ebe9a2f10e86f2d90e567a
Irina Hristova <hristova119@gmail.com>
2017-12-18 17:42:08 +0200

    Solved Problem4.

---
+++ MODULE 1/C# OOP/Extension-Methods-Delegates/AgeRange/Problem4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using FirstBeforeLast;
+using System.Linq;
+namespace AgeRange
+{
+    public class Problem4
+    {
+        static void Main()
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student("Irina", "Hristova", 18));
+            students.Add(new Student("Ivan", "Hristov", 25));
+            students.Add(new Student("Ivan", "Ivanov", 19));
+            FindStudents(students);
+        }
+
+        public static void FindStudents(List<Student> students)
+        {
+            var foundStudents =
+                from student in students
+                where student.Age > 18 && student.Age < 24
+                select student;
+
+            foreach (var student in foundStudents)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} {student.Age}");
+            }
+        }
+    }
+}
