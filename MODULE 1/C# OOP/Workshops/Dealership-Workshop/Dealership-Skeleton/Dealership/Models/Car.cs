d7697d44b0f8c2b5778336c17eb96c1f61db5bec
Irina Hristova <hristova119@gmail.com>
2017-12-20 13:13:28 +0200

    Added new workshop

---
+++ MODULE 1/C# OOP/Workshops/Dealership-Workshop/Dealership-Skeleton/Dealership/Models/Car.cs
@@ -0,0 +1,23 @@
+﻿namespace Dealership.Models
+{
+    public class Car
+    {
+        public Car(string mark, string model, decimal price, int seats)
+        {
+        }
+    }
+
+    public class Truck
+    {
+        public Truck(string make, string model, decimal price, int weightCapacity)
+        {
+        }
+    }
+
+    public class Motorcycle
+    {
+        public Motorcycle(string make, string model, decimal price, string category)
+        {
+        }
+    }
+}
