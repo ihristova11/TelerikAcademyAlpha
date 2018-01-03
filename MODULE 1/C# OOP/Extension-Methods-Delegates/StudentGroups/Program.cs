bb8ee02e6a021445992538b7b2d3da8708f22104
Irina Hristova <hristova119@gmail.com>
2018-01-04 00:14:58 +0200

    Added class Student

---
+++ MODULE 1/C# OOP/Extension-Methods-Delegates/StudentGroups/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentGroups
+{
+    public class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
---
+++ MODULE 1/C# OOP/Extension-Methods-Delegates/StudentGroups/Student.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace StudentGroups
+{
+    public class Student
+    {
+        public Student(string firstName, string lastName, uint fn, string tel, string email, IList<int> marks, uint groupNumber)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.FN = fn;
+            this.Tel = tel;
+            this.Email = email;
+            this.Marks = new List<int>(marks);
+            this.GroupNumber = groupNumber;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public uint FN { get; set; }
+        public string Tel { get; set; }
+        public string Email { get; set; }
+        public IList<int> Marks { get; set; }
+        public uint GroupNumber { get; set; }
+    }
+}
