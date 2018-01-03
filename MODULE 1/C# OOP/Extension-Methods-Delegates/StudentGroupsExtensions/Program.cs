08b379fe4325d11da9b60041c16f05048b7ceb69
Irina Hristova <hristova119@gmail.com>
2018-01-04 00:41:52 +0200

    Solved problem 12

--- MODULE 1/C# OOP/Extension-Methods-Delegates/StudentGroupsExtensions/Program.cs
+++ MODULE 1/C# OOP/Extension-Methods-Delegates/StudentGroupsExtensions/Program.cs
@@ -12,19 +12,24 @@ namespace StudentGroupsExtensions
         {
             var list = new List<Student>()
             {
-                new Student("Irina", "Hristova", 123456, "123456", "someemail@email.com", new List<int>() {6, 6, 6}, 2),
+                new Student("Irina", "Hristova", 123456, "123456", "irina99@abv.bg", new List<int>() {6, 6, 6}, 2),
                 new Student("Ivan", "Hristov", 123456, "02813456", "someemail@email.com", new List<int>() {6, 6, 6}, 3),
                 new Student("Pesho", "Is Back", 123456, "123456", "someemail@email.com", new List<int>() {6, 6, 6}, 2)
             };
 
             foreach (var student in list.FindStudentsWithGroupNumber())
             {
-                Console.WriteLine(student.ToString());
+                Console.WriteLine($"{student.ToString()} <--- student with group number 2");
             }
 
             foreach (var student in list.FindStudentsWithSofiaTel())
             {
-                Console.WriteLine(student.ToString());
+                Console.WriteLine($"{student.ToString()} <--- student Sofia tel");
+            }
+
+            foreach (var student in list.FindStudentsWithEmail("abv.bg"))
+            {
+                Console.WriteLine($"{student.ToString()} <--- student with abv.bg email");
             }
         }
 
@@ -38,7 +43,11 @@ namespace StudentGroupsExtensions
         }
 
         // --- PROBLEM 11 ---
-
+        public static IEnumerable<Student> FindStudentsWithEmail(this IList<Student> studentsList, string email)
+        {
+            var studentsWithGroupNumber2 = studentsList.Where(s => s.Email.Contains("abv.bg"));
+            return studentsWithGroupNumber2;
+        }
 
         // --- PROBLEM 12 ---
         public static IEnumerable<Student> FindStudentsWithSofiaTel(this IList<Student> studentsList)
