45b94beed063b5899b5d2dc353b2611c54c20af1
Irina Hristova <hristova119@gmail.com>
2018-01-06 01:12:08 +0200

    Solved problem 17

---
+++ 01. MODULE 1/C# OOP/Extension-Methods-Delegates/LongestString/LongestString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LongestString
+{
+    public class LongestString
+    {
+        static void Main()
+        {
+            string sentece = "My name is Irinaaaaaaaaaa Hristova.";
+
+            List<string> words = sentece.Split(' ', '.').ToList();
+
+            var wordsLen = words.Select(w => w.Length);
+
+            Console.WriteLine(string.Join(" ", words.Where(w => w.Length == wordsLen.Max())));
+        }
+    }
+}
