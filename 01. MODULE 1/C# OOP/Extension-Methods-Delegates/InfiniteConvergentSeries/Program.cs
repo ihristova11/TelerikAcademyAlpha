1a31b2fa5d46587b4bcc077038ad3682360f2251
Irina Hristova <hristova119@gmail.com>
2018-01-06 01:39:12 +0200

    Solved problem 20*

---
+++ 01. MODULE 1/C# OOP/Extension-Methods-Delegates/InfiniteConvergentSeries/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace InfiniteConvergentSeries
+{
+    public class Program
+    {
+        public delegate double ConvergetSeriesDelegate(int first, double q);
+
+        static void Main()
+        {
+            //int first = int.Parse(Console.ReadLine());
+            //int q = int.Parse(Console.ReadLine());
+
+            int first = 1;
+            double q = 0.5;
+
+            ConvergetSeriesDelegate del = new ConvergetSeriesDelegate(FindSumOfGeometricSequence);
+            var result = del(first, q);
+            Console.WriteLine(result);
+        }
+
+        public static double FindSumOfGeometricSequence(int first, double q)
+        {
+            double result = first / (1 - q);
+            return result;
+        }
+    }
+}
