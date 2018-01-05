6098a487ecc69609ab9e0b6cbd38ea308985cbb1
Irina Hristova <hristova119@gmail.com>
2018-01-05 23:41:52 +0200

    Updated algorithm

---
+++ 02. MODULE 2/Algorithms/01. PrimeNumber/PrimeNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.PrimeNumber
+{
+    public class PrimeNumber
+    {
+        private const int start = 200;
+        private const int end = 300;
+        private static IList<int> primes = new List<int>();
+
+        public static void Main()
+        {
+            for (int n = start; n <= end; n++)
+            {
+                if (IsPrime(n))
+                {
+                    primes.Add(n);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", primes));
+        }
+
+        public static bool IsPrime(int number)
+        {
+            if (number == 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
