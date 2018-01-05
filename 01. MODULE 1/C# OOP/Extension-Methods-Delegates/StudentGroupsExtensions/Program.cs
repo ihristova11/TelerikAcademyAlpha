875eb5d67d7dd0fa7976dc452f7eea2fb74fd6c8
Irina Hristova <hristova119@gmail.com>
2018-01-06 00:27:02 +0200

    Added printing method

--- 01. MODULE 1/C# OOP/Extension-Methods-Delegates/StudentGroupsExtensions/Program.cs
+++ 01. MODULE 1/C# OOP/Extension-Methods-Delegates/StudentGroupsExtensions/Program.cs
@@ -16,20 +16,19 @@ namespace StudentGroupsExtensions
                 new Student("Ivan", "Hristov", 123456, "02813456", "someemail@email.com", new List<int>() {6, 6, 6}, 3),
                 new Student("Pesho", "Is Back", 123456, "123456", "someemail@email.com", new List<int>() {6, 6, 6}, 2)
             };
+            
+            PrintStudents(list.FindStudentsWithGroupNumber());
+            PrintStudents(list.FindStudentsWithEmail("abv.bg"));
+            PrintStudents(list.FindStudentsWithSofiaTel());
+            PrintStudents(list.FindStrudentsWithExcellentMark());
 
-            foreach (var student in list.FindStudentsWithGroupNumber())
-            {
-                Console.WriteLine($"{student.ToString()} <--- student with group number 2");
-            }
+        }
 
-            foreach (var student in list.FindStudentsWithSofiaTel())
+        public static void PrintStudents(IEnumerable<Student> students)
+        {
+            foreach (var student in students)
             {
-                Console.WriteLine($"{student.ToString()} <--- student Sofia tel");
-            }
-
-            foreach (var student in list.FindStudentsWithEmail("abv.bg"))
-            {
-                Console.WriteLine($"{student.ToString()} <--- student with abv.bg email");
+                Console.WriteLine($"{student.ToString()}");
             }
         }
 
@@ -45,15 +44,22 @@ namespace StudentGroupsExtensions
         // --- PROBLEM 11 ---
         public static IEnumerable<Student> FindStudentsWithEmail(this IList<Student> studentsList, string email)
         {
-            var studentsWithGroupNumber2 = studentsList.Where(s => s.Email.Contains("abv.bg"));
-            return studentsWithGroupNumber2;
+            var studentsWithEmail = studentsList.Where(s => s.Email.Contains("abv.bg"));
+            return studentsWithEmail;
         }
 
         // --- PROBLEM 12 ---
         public static IEnumerable<Student> FindStudentsWithSofiaTel(this IList<Student> studentsList)
         {
-            var studentsWithGroupNumber2 = studentsList.Where(s => s.Tel[0] == '0' && s.Tel[1] == '2');
-            return studentsWithGroupNumber2;
+            var studentsWithSofiaTel = studentsList.Where(s => s.Tel[0] == '0' && s.Tel[1] == '2');
+            return studentsWithSofiaTel;
+        }
+
+        // --- PROBLEM 13 ---
+        public static IEnumerable<Student> FindStrudentsWithExcellentMark(this IList<Student> studentsList)
+        {
+            var studentsWithExcellentMarks = studentsList.Where(s => s.Marks.Any(m => m == 6));
+            return studentsWithExcellentMarks; ;
         }
     }
 }
