925518e9f03c6b48647b804a6ea809b50b52e3bd
Irina Hristova <hristova119@gmail.com>
2018-01-08 01:31:38 +0200

    Solving problem.

---
+++ 02. MODULE 2/DataStructuresImplementation/Swappings/Swappings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Swappings
+{
+    public class Swappings
+    {
+        static void Main()
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+            int[] swappings = Console.ReadLine().Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            LinkedList<int> numbers = new LinkedList<int>();
+            numbers.AddFirst(1);
+
+            for (int i = 1; i < numbersCount; i++)
+            {
+                numbers.AddAfter(new LinkedListNode<int>(i - 1), new LinkedListNode<int>(i));
+            }
+            var currValue = numbers.First;
+            int desiredValue = swappings[0];
+
+            for (int i = 0; i < swappings.Length; i++)
+            {
+                while (currValue != null && currValue.Value != desiredValue)
+                {
+                    currValue = currValue.Next;
+                }
+            }
+        }
+    }
+}
