8575728997aa855a960df1828bbf515c0e546edd
Irina Hristova <hristova119@gmail.com>
2018-01-08 23:27:09 +0200

    Solving problem

--- 02. MODULE 2/DataStructuresImplementation/JediMeditation/JediMeditation.cs
+++ 02. MODULE 2/DataStructuresImplementation/JediMeditation/JediMeditation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace JediMeditation
@@ -9,7 +10,85 @@ namespace JediMeditation
         {
             int n = int.Parse(Console.ReadLine());
 
-            string[] jedis = Console.ReadLine().Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+            string[] jedis = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = new LinkedList<string>();
+
+            LinkedListNode<string> master = null;
+            LinkedListNode<string> knight = null;
+            LinkedListNode<string> padawan = null;
+
+            foreach (var jedi in jedis)
+            {
+                switch (jedi[0])
+                {
+                    case 'M':
+                        var newMaster = new LinkedListNode<string>(jedi);
+                        if (master == null)
+                        {
+                            result.AddFirst(newMaster);
+
+                        }
+                        else
+                        {
+                            result.AddAfter(master, newMaster);
+                        }
+                        master = newMaster;
+                        break;
+                    case 'K':
+                        var newKnight = new LinkedListNode<string>(jedi);
+                        if (knight == null)
+                        {
+                            if (master == null)
+                            {
+                                result.AddFirst(newKnight);
+                            }
+                            else
+                            {
+                                result.AddAfter(master, newKnight);
+                            }
+                        }
+                        else
+                        {
+                            result.AddAfter(master, newKnight);
+                        }
+                        knight = newKnight;
+                        break;
+                    case 'P':
+                        var newPadawan = new LinkedListNode<string>(jedi);
+                        if (padawan == null)
+                        {
+                            if (master == null)
+                            {
+                                if (knight == null)
+                                {
+                                    result.AddFirst(newPadawan);
+                                }
+                                else
+                                {
+                                    result.AddAfter(knight, newPadawan);
+                                }
+                            }
+                            else
+                            {
+                                if (knight == null)
+                                {
+                                    result.AddAfter(master, newPadawan);
+                                }
+                                else
+                                {
+                                    result.AddAfter(knight, newPadawan);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            result.AddAfter(padawan, newPadawan);
+                        }
+                        padawan = newPadawan;
+                        break;
+                }
+            }
         }
     }
 }
