e422152ebac55b5245a4810f6a4a5473a9be5993
Irina Hristova <hristova119@gmail.com>
2018-01-10 01:13:50 +0200

    Solved first problem

--- 02. MODULE 2/DataStructuresImplementation/01. SumAndAverageOfElements/SumAndAverageOfElements.cs
+++ 02. MODULE 2/DataStructuresImplementation/01. SumAndAverageOfElements/SumAndAverageOfElements.cs
@@ -1,9 +1,44 @@
-﻿namespace _01.SumAndAverageOfElements
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.SumAndAverageOfElements
 {
     public class SumAndAverageOfElements
     {
         static void Main()
         {
+            List<int> sequence = new List<int>();
+
+            while (true)
+            {
+                try
+                {
+                    sequence.Add(int.Parse(Console.ReadLine()));
+                }
+                catch (Exception e)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(FindSum(sequence));
+            Console.WriteLine(FindAverage(sequence));
+        }
+
+        private static  double FindSum(List<int> sequence)
+        {
+            var sum = 0;
+            foreach (var number in sequence)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
+
+        private static double FindAverage(List<int> sequence)
+        {
+            return FindSum(sequence) / sequence.Count;
         }
     }
 }
