8cca0d98e47c1c156c0b2aa9b49bec6665228d5d
Irina Hristova <hristova119@gmail.com>
2018-01-10 01:22:53 +0200

    Added solution to second problem

---
+++ 02. MODULE 2/DataStructuresImplementation/02. ReverseIntegersWithStack/ReverseIntegersWithStack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.ReverseIntegersWithStack
+{
+    public class ReverseIntegersWithStack
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Stack<int> numbers = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers.Push(int.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
