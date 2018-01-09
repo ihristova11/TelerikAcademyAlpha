c23561b3a178c46d81ef05a3fea37d7f41725608
Irina Hristova <hristova119@gmail.com>
2018-01-10 01:00:31 +0200

    Added solution to problem

---
+++ 02. MODULE 2/Algorithms/MilesToMeters/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MilesToMeters
+{
+    class Program
+    {
+        static void Main()
+        {
+            int miles = int.Parse(Console.ReadLine());
+            Console.WriteLine(miles * 1609);
+        }
+    }
+}
