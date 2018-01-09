5d2e299272a912bd1faf85da4f15e26c555164d9
Irina Hristova <hristova119@gmail.com>
2018-01-10 00:55:39 +0200

    Added solution to problem

---
+++ 02. MODULE 2/Algorithms/MaximalSumOfKElements/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaximalSumOfKElements
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            var numbers = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(numbers);
+
+            int sum = 0;
+            for (int i = numbers.Length - 1; i > numbers.Length - 1 - k; i--)
+            {
+                sum += numbers[i];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
