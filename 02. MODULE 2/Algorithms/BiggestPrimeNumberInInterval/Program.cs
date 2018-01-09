ca9ef15300b8e7f93e18482d424920fd147f43f1
Irina Hristova <hristova119@gmail.com>
2018-01-10 00:34:00 +0200

    Added Sieve of Eratosthenes algorithm for finding biggest prime number

--- 02. MODULE 2/Algorithms/BiggestPrimeNumberInInterval/Program.cs
+++ 02. MODULE 2/Algorithms/BiggestPrimeNumberInInterval/Program.cs
@@ -1,9 +1,40 @@
-﻿namespace BiggestPrimeNumberInInterval
+﻿using System;
+using System.Linq;
+
+namespace BiggestPrimeNumberInInterval
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int n = int.Parse(Console.ReadLine());
+
+            bool[] primes = new bool[n + 1];
+
+            primes[0] = true;
+            primes[1] = true;
+            for (int num = 1; num < primes.Length; num++)
+            {
+                if (primes[num] == false)
+                {
+                    for (int multiplier = 2; multiplier * num < primes.Length; multiplier++)
+                    {
+                        primes[multiplier * num] = true;
+                    }
+                }
+            }
+
+            int x = primes.Length - 1;
+
+            while (true)
+            {
+                if (primes[x] == false)
+                {
+                    Console.WriteLine(x);
+                    break;
+                }
+                --x;
+            }
         }
     }
 }
