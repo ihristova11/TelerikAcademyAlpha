66b37a0208c6d1cd8262d2e72065bc5a7cb1eb52
Irina Hristova <hristova119@gmail.com>
2018-01-10 10:22:55 +0200

    Added solution to problem 4

--- 02. MODULE 2/DataStructuresImplementation/04. LongestSubsequence/LongestSubsequence.cs
+++ 02. MODULE 2/DataStructuresImplementation/04. LongestSubsequence/LongestSubsequence.cs
@@ -1,9 +1,36 @@
-﻿namespace _04.LongestSubsequence
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.LongestSubsequence
 {
     class LongestSubsequence
     {
         static void Main()
         {
+            List<int> seq = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            Console.WriteLine(FindLongest(seq));
+        }
+
+        private static int FindLongest(List<int> seq)
+        {
+            int best = 1;
+            int counter = 1;
+            for (int i = 1; i < seq.Count; i++)
+            {
+                if (seq[i - 1] == seq[i])
+                {
+                    counter++;
+
+                    best = best < counter ? counter : best;
+                }
+                else
+                {
+                    counter = 1;
+                }
+            }
+
+            return best;
         }
     }
 }
