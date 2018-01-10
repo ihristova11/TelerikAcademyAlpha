c1f2f432a000184973fd77bfd47982c02c0f52ae
Irina Hristova <hristova119@gmail.com>
2018-01-11 00:17:15 +0200

    Added linear search algorithm

---
+++ 02. MODULE 2/Searching-Algorithms/SortableCollection/SortableCollection.cs
@@ -0,0 +1,23 @@
+﻿namespace SortableCollection
+{
+    class SortableCollection
+    {
+        static void Main()
+        {
+            int[] numbers = {1, 5, 4, 2, 7, -1, 0};
+        }
+
+        public static int LinearSearch(int[] numbers, int desired)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == desired)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
