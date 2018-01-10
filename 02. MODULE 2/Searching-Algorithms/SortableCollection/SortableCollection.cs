ba5e0bef367cc44efdc04e19d19b7301c08b7b0a
Irina Hristova <hristova119@gmail.com>
2018-01-11 00:31:36 +0200

    Added binary search algorithm

--- 02. MODULE 2/Searching-Algorithms/SortableCollection/SortableCollection.cs
+++ 02. MODULE 2/Searching-Algorithms/SortableCollection/SortableCollection.cs
@@ -1,10 +1,16 @@
-﻿namespace SortableCollection
+﻿using System;
+using System.IO;
+
+namespace SortableCollection
 {
     class SortableCollection
     {
         static void Main()
         {
-            int[] numbers = {1, 5, 4, 2, 7, -1, 0};
+            int[] numbers = { 1, 5, 4, 2, 7, -1, 0 };
+            int[] sortedNumbers = { 1, 2, 4, 6, 8, 11, 17 };
+            Console.WriteLine(LinearSearch(numbers, 7));
+            Console.WriteLine(BinarySearch(sortedNumbers, 0, sortedNumbers.Length - 1, 6));
         }
 
         public static int LinearSearch(int[] numbers, int desired)
@@ -18,6 +24,34 @@
             }
 
             return -1;
+        }
+
+        public static int BinarySearch(int[] numbers, int min, int max, int desired)
+        {
+            int mid = (min + max) / 2;
+            int midValue = numbers[mid];
+
+            if (min > max)
+            {
+                return -1;
+            }
+            else
+            {
+                if (desired == midValue)
+                {
+                    return mid;
+                }
+                else if (desired > midValue)
+                {
+                    min = mid + 1;
+                }
+                else
+                {
+                    max = mid - 1;
+                }
+
+                return BinarySearch(numbers, min, max, desired);
+            }
         }
     }
 }
