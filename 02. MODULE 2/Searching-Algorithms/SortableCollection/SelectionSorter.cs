d5a7720cfba72c2a395c1ca6dbb6013e7f4a538b
Irina Hristova <hristova119@gmail.com>
2018-01-11 23:58:27 +0200

    Implementing SelectionSorter

--- 02. MODULE 2/Searching-Algorithms/SortableCollection/SelectionSorter.cs
+++ 02. MODULE 2/Searching-Algorithms/SortableCollection/SelectionSorter.cs
@@ -1,10 +1,34 @@
-﻿namespace SortableCollection
+﻿using System;
+
+namespace SortableCollection
 {
     public class SelectionSorter : ISorter
     {
         public void Sort()
         {
-            
+            SelectionSort(new SortableCollection(new int[] { 1, 5, 4, 2, 7, -1, 0 }));
+        }
+
+        public void SelectionSort(SortableCollection collection)
+        {
+            int min;
+            int temp;
+            for (int i = 0; i < collection.Numbers.Length; i++)
+            {
+                min = collection.Numbers[i];
+
+                for (int j = i + 1; j < collection.Numbers.Length - 1; j++)
+                {
+                    if (min > collection.Numbers[j])
+                    {
+                        min = collection.Numbers[j];
+                    }
+
+                    //swappint elements
+                    collection.Numbers[i] = collection.Numbers[j];
+                    collection.Numbers[j] = min;
+                }
+            }
         }
     }
 }
