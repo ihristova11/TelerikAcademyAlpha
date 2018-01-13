7714e58aac21d66adbdd53e7cce1c1242e772e69
Irina Hristova <hristova119@gmail.com>
2018-01-13 17:21:00 +0200

    Solved CokiSkoki

--- 02. MODULE 2/DataStructuresImplementation/CokiSkoki/CokiSkoki.cs
+++ 02. MODULE 2/DataStructuresImplementation/CokiSkoki/CokiSkoki.cs
@@ -1,9 +1,43 @@
-﻿namespace CokiSkoki
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CokiSkoki
 {
     public class CokiSkoki
     {
         static void Main()
         {
+            //get the input data
+            int n = int.Parse(Console.ReadLine());
+
+            int[] buildingsHeight = Console.ReadLine()
+                .Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> peekIndices = new Stack<int>();
+            int[] pathLengths = new int[n];
+
+            for (int index = buildingsHeight.Length - 1; index >= 0; index--)
+            {
+                while (peekIndices.Count != 0 && buildingsHeight[peekIndices.Peek()] < buildingsHeight[index] )
+                {
+                    int peekIndex = peekIndices.Pop();
+                    pathLengths[peekIndex] = peekIndices.Count();
+                }
+
+                peekIndices.Push(index);
+            }
+
+            while (peekIndices.Count != 0)
+            {
+                int peekIndex = peekIndices.Pop();
+                pathLengths[peekIndex] = peekIndices.Count();
+            }
+
+            Console.WriteLine(pathLengths.Max());
+            Console.WriteLine(string.Join(" ", pathLengths));
         }
     }
 }
