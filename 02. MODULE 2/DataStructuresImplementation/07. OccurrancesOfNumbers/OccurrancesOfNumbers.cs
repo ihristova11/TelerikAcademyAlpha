6fc3b71d31c6117de90feb48f0a015d14195a3e0
Irina Hristova <hristova119@gmail.com>
2018-01-14 00:40:42 +0200

    Used SortedDictionary to solve problem 7

--- 02. MODULE 2/DataStructuresImplementation/07. OccurrancesOfNumbers/OccurrancesOfNumbers.cs
+++ 02. MODULE 2/DataStructuresImplementation/07. OccurrancesOfNumbers/OccurrancesOfNumbers.cs
@@ -1,9 +1,47 @@
-﻿namespace _07.OccurrancesOfNumbers
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.OccurrancesOfNumbers
 {
     public class OccurrancesOfNumbers
     {
         static void Main()
         {
+            int[] array = new int[]{ 3, 4, 4, 2, 3, 3, 4, 3, 2 };
+
+            PrintDictionary(CountOccurrances(array));
+        }
+
+        public static SortedDictionary<int, int> CountOccurrances(int[] arr)
+        {
+            if (arr.Length == 0)
+            {
+                throw new ArgumentException("array cannot be empty");
+            }
+
+            SortedDictionary<int, int> numbersOccurrances = new SortedDictionary<int, int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (numbersOccurrances.ContainsKey(arr[i]))
+                {
+                    numbersOccurrances[arr[i]]++;
+                }
+                else
+                {
+                    numbersOccurrances.Add(arr[i], 1);
+                }
+            }
+
+            return numbersOccurrances;
+        }
+
+        public static void PrintDictionary(SortedDictionary<int, int> dictionary)
+        {
+            foreach (var element in dictionary)
+            {
+                Console.WriteLine($"{element.Key} --> {element.Value} times");
+            }
         }
     }
 }
