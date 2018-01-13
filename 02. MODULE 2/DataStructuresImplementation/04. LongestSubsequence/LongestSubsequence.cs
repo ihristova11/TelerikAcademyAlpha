5d0aa924bd5328be79b7717285d340906415a8ad
Irina Hristova <hristova119@gmail.com>
2018-01-14 00:31:36 +0200

    Solved RemoveOddOccurrances

--- 02. MODULE 2/DataStructuresImplementation/04. LongestSubsequence/LongestSubsequence.cs
+++ 02. MODULE 2/DataStructuresImplementation/04. LongestSubsequence/LongestSubsequence.cs
@@ -8,54 +8,7 @@ namespace _04.LongestSubsequence
     {
         static void Main()
         {
-            List<int> numbers = new List<int>() { 4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2 };
-            Console.WriteLine(string.Join(" ", numbers));
-
-            Console.WriteLine(string.Join(" ", RemoveOddOccurrances(numbers)));
-        }
-
-        public static Dictionary<int, int> CountOccurrances(IList<int> numbers)
-        {
-            if (numbers == null || numbers.Count == 0)
-            {
-                throw new ArgumentException("cannot be empty");
-            }
-
-            Dictionary<int, int> dictionary = new Dictionary<int, int>();
             
-            for (int i = 0; i < numbers.Count; i++)
-            {
-                if (dictionary.ContainsKey(numbers[i]))
-                {
-                    dictionary[numbers[i]]++;
-                }
-                else
-                {
-                    dictionary.Add(numbers[i], 1);
-                }
-            }
-
-            return dictionary;
-        }
-
-        public static IList<int> RemoveOddOccurrances(List<int> numbers)
-        {
-            if (numbers == null || numbers.Count == 0)
-            {
-                throw new ArgumentException("cannot be empty");
-            }
-
-            Dictionary<int, int> occurrances = CountOccurrances(numbers);
-
-            foreach (var element in occurrances)
-            {
-                if (element.Value % 2 == 1)
-                {
-                    numbers.RemoveAll(e => e == element.Key);
-                }
-            }
-
-            return numbers;
         }
     }
 }
--- 02. MODULE 2/DataStructuresImplementation/06. RemoveOddOccurrences/RemoveOddOccurrences.cs
+++ 02. MODULE 2/DataStructuresImplementation/06. RemoveOddOccurrences/RemoveOddOccurrences.cs
@@ -9,32 +9,54 @@ namespace _06.RemoveOddOccurrences
     {
         static void Main()
         {
-            //todo: fix!!!
-            var list = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
-            var occur = new Queue<int>();
-            var numbers = new Queue<int>();
+            List<int> numbers = new List<int>() { 4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2 };
+            Console.WriteLine(string.Join(" ", numbers));
 
-            list.Sort();
-            Console.WriteLine(string.Join(" ", list));
-            int counter = 1;
-            for (int i = 1; i < list.Count; i++)
+            Console.WriteLine(string.Join(" ", RemoveOddOccurrances(numbers)));
+        }
+
+        public static Dictionary<int, int> CountOccurrances(IList<int> numbers)
+        {
+            if (numbers == null || numbers.Count == 0)
             {
-                if (list[i - 1] == list[i])
-                {
-                    counter++;
-                }
-                else
-                {
-                    numbers.Enqueue(i);
-                    occur.Enqueue(counter);
-                    counter = 1;
-                }
+                throw new ArgumentException("cannot be empty");
             }
+
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
 
             for (int i = 0; i < numbers.Count; i++)
             {
-                Console.WriteLine($"{numbers.Dequeue()} - {occur.Dequeue()}");
+                if (dictionary.ContainsKey(numbers[i]))
+                {
+                    dictionary[numbers[i]]++;
+                }
+                else
+                {
+                    dictionary.Add(numbers[i], 1);
+                }
             }
+
+            return dictionary;
+        }
+
+        public static IList<int> RemoveOddOccurrances(List<int> numbers)
+        {
+            if (numbers == null || numbers.Count == 0)
+            {
+                throw new ArgumentException("cannot be empty");
+            }
+
+            Dictionary<int, int> occurrances = CountOccurrances(numbers);
+
+            foreach (var element in occurrances)
+            {
+                if (element.Value % 2 == 1)
+                {
+                    numbers.RemoveAll(e => e == element.Key);
+                }
+            }
+
+            return numbers;
         }
     }
 }
