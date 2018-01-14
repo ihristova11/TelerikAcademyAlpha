f437b8f2f236c8351f0309baa8d2b6c884194cb5
Irina Hristova <hristova119@gmail.com>
2018-01-14 02:36:22 +0200

    Solved problem 10.

---
+++ 02. MODULE 2/DataStructuresImplementation/10. ShortestSequenceOfOperations/ShortestSequenceOfOperations.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10.ShortestSequenceOfOperations
+{
+    public class ShortestSequenceOfOperations
+    {
+        static void Main()
+        {
+            Queue<int> operations = new Queue<int>();
+
+            int startNumber = 5;
+            int endNumber = 16;
+
+            while (startNumber <= endNumber)
+            {
+                operations.Enqueue(endNumber);
+
+                if (endNumber / 2 >= startNumber)
+                {
+                    if (endNumber % 2 == 0)
+                    {
+                        endNumber /= 2;
+                    }
+                    else
+                    {
+                        endNumber--;
+                    }
+                }
+                else
+                {
+                    if (endNumber - 2 >= startNumber)
+                    {
+                        endNumber -= 2;
+                    }
+                    else
+                    {
+                        endNumber--;
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" -> ", operations.Reverse()));
+        }
+    }
+}
