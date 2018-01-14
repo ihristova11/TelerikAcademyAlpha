245206f48cc5e4617b21ab5436294672b0fc14f9
Irina Hristova <hristova119@gmail.com>
2018-01-14 02:40:26 +0200

    Added solution to problem 4

--- 02. MODULE 2/DataStructuresImplementation/04. LongestSubsequence/LongestSubsequence.cs
+++ 02. MODULE 2/DataStructuresImplementation/04. LongestSubsequence/LongestSubsequence.cs
@@ -6,9 +6,61 @@ namespace _04.LongestSubsequence
 {
     public class LongestSubsequence
     {
-        static void Main()
+        public static void Main()
         {
-            
+            Console.WriteLine("Input numbers: ");
+            var numbers = new List<int>();
+            var inputLine = Console.ReadLine();
+
+            while (inputLine != string.Empty)
+            {
+                var number = int.Parse(inputLine);
+                numbers.Add(number);
+                inputLine = Console.ReadLine();
+            }
+
+            Console.WriteLine("The longest sequence of equal numbers is: ");
+            var list = GetLongestEqualSubsequence(numbers);
+            list.ForEach(n => Console.Write("{0} ", n));
+            Console.WriteLine();
+        }
+
+        public static List<int> GetLongestEqualSubsequence(IList<int> numbersList)
+        {
+            var longestSubsequence = 0;
+            var currentSubsequence = 0;
+            var subsequenceNumber = 0;
+
+            for (var i = 0; i < numbersList.Count(); i++)
+            {
+                var currentElement = numbersList[i];
+
+                if (i == 0)
+                {
+                    currentSubsequence = 1;
+                }
+                else
+                {
+                    var previousElement = numbersList[i - 1];
+
+                    if (currentElement != previousElement)
+                    {
+                        currentSubsequence = 1;
+                    }
+                    else
+                    {
+                        ++currentSubsequence;
+                    }
+                }
+
+                if (currentSubsequence > longestSubsequence)
+                {
+                    longestSubsequence = currentSubsequence;
+                    subsequenceNumber = currentElement;
+                }
+            }
+
+            return Enumerable.Repeat(subsequenceNumber, longestSubsequence).ToList();
         }
     }
 }
