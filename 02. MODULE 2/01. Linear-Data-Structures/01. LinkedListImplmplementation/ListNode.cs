9aa75379a9c4eee3c0dd80b823e32450531a2b37
Irina Hristova <hristova119@gmail.com>
2018-01-14 03:10:30 +0200

    Implementing LunkedList<T> and LinkedListNode<T>

---
+++ 02. MODULE 2/01. Linear-Data-Structures/01. LinkedListImplmplementation/ListNode.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.LinkedListImplmplementation
+{
+    public class ListNode<T>
+    {
+        public ListNode(T value)
+        {
+            this.Value = value;
+        }
+        
+        public T Value { get; set; }
+
+        public ListNode<T> Next { get; set; }
+
+        public ListNode<T> Prev { get; set; }
+    }
+}
---
+++ 02. MODULE 2/01. Linear-Data-Structures/01. LinkedListImplmplementation/NodesList.cs
@@ -0,0 +1,43 @@
+﻿namespace _01.LinkedListImplmplementation
+{
+    public class NodesList<T>
+    {
+
+        public ListNode<T> Head { get; set; }
+
+        public ListNode<T> First { get; set; }
+
+        public ListNode<T> Last { get; set; }
+
+        public int Count { get; set; }
+
+        public void AddBefore(ListNode<T> node, ListNode<T> newNode)
+        {
+            newNode.Next = node;
+            newNode.Prev = node.Prev;
+            newNode.Prev.Next = newNode;
+            node.Prev = newNode;
+
+            this.Count++;
+        }
+
+        public void Remove(ListNode<T> nodeToRemove)
+        {
+            if (nodeToRemove.Next != nodeToRemove)
+            {
+                nodeToRemove.Next.Prev = nodeToRemove.Prev;
+                nodeToRemove.Prev.Next = nodeToRemove.Next;
+                if (this.Head == nodeToRemove)
+                {
+                    this.Head = nodeToRemove.Next;
+                }
+            }
+            else
+            {
+                this.Head = null;
+            }
+
+            this.Count--;
+        }
+    }
+}
