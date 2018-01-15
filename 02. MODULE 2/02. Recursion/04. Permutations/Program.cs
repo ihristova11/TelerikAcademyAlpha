acffa247dc61e59cacf833d177966dd22f891c41
Irina Hristova <hristova119@gmail.com>
2018-01-15 02:08:48 +0200

    Added permutations generator

---
+++ 02. MODULE 2/02. Recursion/04. Permutations/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.SqlServer.Server;
+
+namespace _04.Permutations
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool[] used = new bool[n];
+            int[] mp = new int[n];
+
+            Permute(0, n, used, mp);
+        }
+
+        public static void Permute(int i, int n, bool[] used, int[] mp)
+        {
+            int k;
+            if (i >= n)
+            {
+                Print(n, mp);
+                return;
+            }
+            for (k = 0; k < n; k++)
+            {
+                if (!used[k])
+                {
+                    used[k] = true;
+                    mp[i] = k + 1;
+                    Permute(i + 1, n, used, mp);
+                    used[k] = false;
+                }
+            }
+        }
+
+        public static void Print(int n, int[] mp)
+        {
+            Console.WriteLine(string.Join(" ", mp));
+        }
+    }
+}
