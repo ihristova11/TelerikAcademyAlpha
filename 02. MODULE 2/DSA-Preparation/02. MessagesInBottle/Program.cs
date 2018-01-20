b5725f71c2316fbac6a79399c8d09f554678e20c
Irina Hristova <hristova119@gmail.com>
2018-01-20 11:01:24 +0200

    Added problem Messages in Bottle

---
+++ 02. MODULE 2/DSA-Preparation/02. MessagesInBottle/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _02.MessagesInBottle
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string secretCode = Console.ReadLine();
+            string cipher = Console.ReadLine();
+        }
+    }
+}
