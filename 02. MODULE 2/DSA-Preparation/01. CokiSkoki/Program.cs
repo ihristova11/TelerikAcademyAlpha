1da89e10331845a0a7de0468448675ebaaec4067
Irina Hristova <hristova119@gmail.com>
2018-01-20 10:59:40 +0200

    Solved CokiSkoki with Stack

---
+++ 02. MODULE 2/DSA-Preparation/01. CokiSkoki/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CokiSkoki
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var buildings = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] sequenceLens = new int[n];
+            Stack<int> peeks = new Stack<int>();
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                while (peeks.Count != 0 && buildings[peeks.Peek()] <= buildings[i])
+                {
+                    int peekInd = peeks.Pop();
+                    sequenceLens[peekInd] = peeks.Count;
+                }
+
+                peeks.Push(i);
+            }
+
+            while (peeks.Count != 0)
+            {
+                int peekInd = peeks.Pop();
+                sequenceLens[peekInd] = peeks.Count;
+            }
+
+            Console.WriteLine(sequenceLens.Max());
+            Console.WriteLine(string.Join(" ", sequenceLens));
+        }
+    }
+}
