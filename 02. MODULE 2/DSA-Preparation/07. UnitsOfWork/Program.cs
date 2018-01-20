b43d3a324be5a08b4c4e575995742e43b450d152
Irina Hristova <hristova119@gmail.com>
2018-01-20 19:57:49 +0200

    Added Slogan

--- 02. MODULE 2/DSA-Preparation/07. UnitsOfWork/Program.cs
+++ 02. MODULE 2/DSA-Preparation/07. UnitsOfWork/Program.cs
@@ -1,16 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace _07.UnitsOfWork
 {
     public class Unit : IComparable<Unit>
     {
+        public Unit(string name, string type, int attack)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Attack = attack;
+        }
+
         public string Name { get; set; }
         public string Type { get; set; }
         public int Attack { get; set; }
 
         public int CompareTo(Unit other)
         {
-            throw new NotImplementedException();
+            int result = this.Attack.CompareTo(other.Attack) * -1;
+            if (result == 0)
+            {
+                result = this.Name.CompareTo(other.Name);
+            }
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}[{1}]({2})", this.Name, this.Type, this.Attack);
         }
     }
 
@@ -18,6 +39,95 @@ namespace _07.UnitsOfWork
     {
         static void Main()
         {
+            Dictionary<string, SortedSet<Unit>> unitsByType = new Dictionary<string, SortedSet<Unit>>();
+            Dictionary<string, Unit> unitsByName = new Dictionary<string, Unit>();
+            SortedSet<Unit> unitsOrderedByAttack = new SortedSet<Unit>();
+            StringBuilder resultBuilder = new StringBuilder();
+
+            string line;
+            while ((line = Console.ReadLine()) != "end")
+            {
+                var commandParameters = line
+                    .Split(' ')
+                    .ToArray();
+
+                string choice = commandParameters[0];
+
+                switch (choice)
+                {
+                    case "add":
+                        string name = commandParameters[1];
+                        string type = commandParameters[2];
+                        int attack = int.Parse(commandParameters[3]);
+
+                        Unit unit = new Unit(name, type, attack);
+
+                        if (unitsByName.ContainsKey(unit.Name))
+                        {
+                            resultBuilder.AppendFormat("FAIL: {0} already exists!", unit.Name);
+                            Console.WriteLine(resultBuilder);
+                            resultBuilder.Clear();
+                        }
+                        else
+                        {
+                            unitsByName.Add(unit.Name, unit);
+                            unitsOrderedByAttack.Add(unit);
+                            if (!unitsByType.ContainsKey(unit.Type))
+                            {
+                                unitsByType.Add(unit.Type, new SortedSet<Unit>());
+                            }
+
+                            unitsByType[unit.Type].Add(unit);
+
+                            resultBuilder.AppendFormat("SUCCESS: {0} added!", unit.Name);
+                            Console.WriteLine(resultBuilder);
+                            resultBuilder.Clear();
+                        }
+                        break;
+                    case "remove":
+                        string removeName = commandParameters[1];
+                        if (unitsByName.ContainsKey(removeName))
+                        {
+                            Unit unitToRemove = unitsByName[removeName];
+                            unitsByName.Remove(removeName);
+                            unitsByType[unitToRemove.Type].Remove(unitToRemove);
+                            unitsOrderedByAttack.Remove(unitsOrderedByAttack.First(x => x.Name == removeName));
+                            resultBuilder.AppendFormat("SUCCESS: {0} removed!", removeName);
+                            Console.WriteLine(resultBuilder);
+                            resultBuilder.Clear();
+                        }
+                        else
+                        {
+                            resultBuilder.AppendFormat("FAIL: {0} could not be found!", removeName);
+                            Console.WriteLine(resultBuilder);
+                            resultBuilder.Clear();
+                        }
+                        break;
+                    case "find":
+                        string findType = commandParameters[1];
+                        if (unitsByType.ContainsKey(findType))
+                        {
+                            var found = unitsByType[findType].Take(10);
+                            resultBuilder.AppendFormat("RESULT: {0}", string.Join(", ", found));
+                            Console.WriteLine(resultBuilder);
+                            resultBuilder.Clear();
+                        }
+                        else
+                        {
+                            resultBuilder.Append("RESULT: ");
+                            Console.WriteLine(resultBuilder);
+                            resultBuilder.Clear();
+                        }
+                        break;
+                    case "power":
+                        int numberOfUnits = int.Parse(commandParameters[1]);
+                        var powerUnits = unitsOrderedByAttack.Take(numberOfUnits);
+                        resultBuilder.AppendFormat("RESULT: {0}", string.Join(", ", powerUnits));
+                        Console.WriteLine(resultBuilder);
+                        resultBuilder.Clear();
+                        break;
+                }
+            }
         }
     }
 }
