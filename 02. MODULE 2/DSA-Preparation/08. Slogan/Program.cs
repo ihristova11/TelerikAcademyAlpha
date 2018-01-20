914f0e595c727d506f3a022e60271658f08c9af0
Irina Hristova <hristova119@gmail.com>
2018-01-20 22:07:11 +0200

    Added solution to problem

--- 02. MODULE 2/DSA-Preparation/08. Slogan/Program.cs
+++ 02. MODULE 2/DSA-Preparation/08. Slogan/Program.cs
@@ -1,15 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _08.Slogan
+namespace Slogan
 {
-    class Program
+    // similar to author's solution
+    // similar to Message in Bottle solution
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var suggestedWords = Console.ReadLine()
+                    .Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+                string sloganToTry = Console.ReadLine();
+
+                List<string> usedWords = new List<string>();
+                HashSet<string> impossibles = new HashSet<string>();
+                if (GenerateVariants(suggestedWords, sloganToTry, usedWords, impossibles))
+                {
+                    usedWords.Reverse();
+                    Console.WriteLine(string.Join(" ", usedWords));
+                }
+                else
+                {
+                    Console.WriteLine("NOT VALID");
+                }
+            }
+        }
+
+        private static bool GenerateVariants(IList<string> suggestions, string sloganToTry,
+            IList<string> usedWords, HashSet<string> impossibles)
+        {
+            if (sloganToTry.Length < 1)
+            {
+                // bottom of recursion
+                return true;
+            }
+
+            if (impossibles.Contains(sloganToTry))
+            {
+                // optimization for time
+                // stops the recursion
+                return false; 
+            }
+
+            foreach (var word in suggestions)
+            {
+                if (sloganToTry.StartsWith(word))
+                {
+                    string subSlogan = sloganToTry.Substring(word.Length);
+
+                    if (GenerateVariants(suggestions, subSlogan, usedWords, impossibles))
+                    {
+                        usedWords.Add(word);
+                        return true;
+                    }
+                }
+            }
+            impossibles.Add(sloganToTry);
+            return false;
         }
     }
 }
+
+
