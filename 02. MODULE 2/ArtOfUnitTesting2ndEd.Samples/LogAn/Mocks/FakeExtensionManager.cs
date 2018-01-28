e77fa6718f2dfec54a9b07e94dd52fd0555769ab
Irina Hristova <hristova119@gmail.com>
2018-01-28 10:57:53 +0200

    Added FakeExtensionManager

---
+++ 02. MODULE 2/ArtOfUnitTesting2ndEd.Samples/LogAn/Mocks/FakeExtensionManager.cs
@@ -0,0 +1,11 @@
+﻿namespace LogAn.Mocks
+{
+    public class FakeExtensionManager : IExtensionManager
+    {
+        public bool WillBeValid = false;
+        public bool IsValid(string fileName)
+        {
+            return WillBeValid;
+        }
+    }
+}
