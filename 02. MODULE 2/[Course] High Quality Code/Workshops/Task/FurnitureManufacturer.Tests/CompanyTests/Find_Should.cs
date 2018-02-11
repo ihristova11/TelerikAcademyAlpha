25205a0abdce1e1c879a95e0c258f0c634ec456b
Irina Hristova <hristova119@gmail.com>
2018-02-11 15:02:04 +0200

    Added tests

--- 02. MODULE 2/[Course] High Quality Code/Workshops/Task/FurnitureManufacturer.Tests/CompanyTests/Find_Should.cs
+++ 02. MODULE 2/[Course] High Quality Code/Workshops/Task/FurnitureManufacturer.Tests/CompanyTests/Find_Should.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using FurnitureManufacturer.Interfaces;
 using FurnitureManufacturer.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace FurnitureManufacturer.Tests.CompanyTests
 {
@@ -24,13 +26,35 @@ namespace FurnitureManufacturer.Tests.CompanyTests
         [TestMethod]
         public void ReturnNull_IfFurnitureNotFound()
         {
+            // Arrange
             var model = "model";
             string name = "somecompanynamehere";
             string registrationNumber = "1234567899";
 
             var sut = new Company(name, registrationNumber);
 
+            // Act & Assert
             Assert.IsTrue(null == sut.Find(model));
+        }
+
+        [TestMethod]
+        public void ReturnFurniture_WhenThereIsSuchInCollection()
+        {
+            // Arrange
+            var model = "model";
+            string name = "somecompanynamehere";
+            string registrationNumber = "1234567899";
+            var furnitureStub = new Mock<IFurniture>();
+            furnitureStub.Setup(x => x.Model).Returns(model);
+            var sut = new Company(name, registrationNumber);
+            sut.Furnitures.Clear();
+            sut.Furnitures.Add(furnitureStub.Object);
+
+            // Act
+            var result = sut.Find(model);
+
+            // Assert
+            Assert.AreSame(furnitureStub.Object, result);
         }
     }
 }
