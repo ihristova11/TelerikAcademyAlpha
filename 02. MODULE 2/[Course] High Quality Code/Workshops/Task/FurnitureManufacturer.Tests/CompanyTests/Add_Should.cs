40a3d31b9a2edd451a4f605d9436d07711ce6408
Irina Hristova <hristova119@gmail.com>
2018-02-11 14:42:40 +0200

    Added new tests for Add and Find

--- 02. MODULE 2/[Course] High Quality Code/Workshops/Task/FurnitureManufacturer.Tests/CompanyTests/Add_Should.cs
+++ 02. MODULE 2/[Course] High Quality Code/Workshops/Task/FurnitureManufacturer.Tests/CompanyTests/Add_Should.cs
@@ -1,9 +1,25 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using FurnitureManufacturer.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FurnitureManufacturer.Tests.CompanyTests
 {
     [TestClass]
     public class Add_Should
     {
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenFurnitureIsNull()
+        {
+            // Arrange
+            var companyName = "somecompanyname";
+            var registrationNumber = "1234567890";
+
+            var sut = new Company(companyName, registrationNumber);
+
+            // Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => sut.Add(null));
+        }
+
+
     }
 }
--- 02. MODULE 2/[Course] High Quality Code/Workshops/Task/FurnitureManufacturer.Tests/CompanyTests/Find_Should.cs
+++ 02. MODULE 2/[Course] High Quality Code/Workshops/Task/FurnitureManufacturer.Tests/CompanyTests/Find_Should.cs
@@ -1,9 +1,24 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using FurnitureManufacturer.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FurnitureManufacturer.Tests.CompanyTests
 {
     [TestClass]
     public class Find_Should
     {
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenCompanyModelIsNull()
+        {
+            // Arrange
+            string name = "somecompanynamehere";
+            string registrationNumber = "1234567899";
+            string model = null;
+
+            var sut = new Company(name, registrationNumber);
+
+            // Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => sut.Find(model));
+        }
     }
 }
