32b2e7bf2060ee66ddc1bc96a9b54cb1e544b9c8
Irina Hristova <hristova119@gmail.com>
2018-02-15 01:55:07 +0200

    Added solutions

---
+++ 02. MODULE 2/[Course] DSA/JudgeProblems/FirstLargestThanNeighbours/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _06.FirstLargerThanNeighbours
+{
+    using System;
+    using System.Linq;
+
+    class FirstLargerThanNeighbours
+    {
+        static void Main()
+        {
+            Console.WriteLine(FindFirstLarger());
+        }
+
+        public static int FindFirstLarger()
+        {
+            int len = int.Parse(Console.ReadLine());
+            string line = Console.ReadLine();
+            int[] numbers = line.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            for (int i = 1; i < len - 1; i++)
+            {
+                if (numbers[i - 1] < numbers[i] && numbers[i] > numbers[i + 1])
+                {
+                    return i;
+                    break;
+                }
+
+            }
+            return -1;
+        }
+    }
+}
