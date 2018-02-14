2e4bae276befb5b4dddc5f3365a40f78a3347788
Irina Hristova <hristova119@gmail.com>
2018-02-15 01:22:59 +0200

    Added solution

---
+++ 02. MODULE 2/[Course] DSA/JudgeProblems/Trapezoid/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Trapezoid
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(new string('.', n) + new string('*', n));
+            for (int i = 1; i < n; i++)
+            {
+                Console.WriteLine(new string('.', n - i) + "*" + new string('.', n + i - 2) + "*");
+            }
+
+            Console.WriteLine(new string('*', 2 * n));
+        }
+    }
+}
