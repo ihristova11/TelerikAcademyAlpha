6b35999f641ee347933de7b904a9b2e051a605b7
Irina Hristova <hristova119@gmail.com>
2018-02-15 01:07:54 +0200

    Added solution

---
+++ 02. MODULE 2/[Course] DSA/JudgeProblems/BonusScore/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BonusScore
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int score = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(AddBonus(score));
+        }
+
+        static string AddBonus(int score)
+        {
+            if (score >= 1 && score <= 3)
+            {
+                score *= 10;
+            }
+            else if (score >= 4 && score <= 6)
+            {
+                score *= 100;
+            }
+            else if (score >= 7 && score <= 9)
+            {
+                score *= 1000;
+            }
+            else if (score <= 0 || score > 9)
+            {
+                return "invalid score";
+            }
+
+            return score.ToString();
+        }
+    }
+}
---
+++ 02. MODULE 2/[Course] DSA/JudgeProblems/TribonacciTriangle/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        long firstNum = long.Parse(Console.ReadLine());
+        long secondNum = long.Parse(Console.ReadLine());
+        long thirdNum = long.Parse(Console.ReadLine());
+        long l = long.Parse(Console.ReadLine());
+        long prevF;
+        long prevS;
+        long prevT;
+        long temp, tempS;
+        long[] arr = new long[20];
+        string printLine = "";
+        prevF = firstNum + secondNum + thirdNum;
+        prevS = secondNum + thirdNum + prevF;
+        prevT = thirdNum + prevF + prevS;
+        //print first and second line
+        Console.WriteLine(firstNum);
+        Console.WriteLine(secondNum + " " + thirdNum);
+
+        if (l > 2)
+        {
+            Console.WriteLine(prevF + " " + prevS + " " + prevT);
+            for (int i = 4; i <= l; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    //prevF = prevF + prevS + prevT;
+                    //prevS = prevF + prevS + prevT;
+                    //prevT = prevF + prevS + prevT;
+                    arr[j] = prevF + prevS + prevT;
+                    temp = prevT;
+                    tempS = prevS;
+                    prevT = arr[j];
+                    prevS = temp;
+                    prevF = tempS;
+                    if (j == i - 1)
+                    {
+                        printLine += arr[j];
+                    }
+                    else
+                    {
+                        printLine += arr[j] + " ";
+                    }
+                    //string.Format(" ", arr[j]);
+                }
+                Console.WriteLine(printLine);
+                printLine = "";
+                //Console.WriteLine(prevF + " " + prevS + " " + prevT); // print the line
+            }
+        }
+    }
+}
