4f4496f8e10abd037f86a63c38290f14143663b6
Irina Hristova <hristova119@gmail.com>
2018-02-15 00:39:36 +0200

    Added solution

---
+++ 02. MODULE 2/[Course] DSA/JudgeProblems/CheckForPlayCard/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CheckForPlayCard
+{
+    public class Program
+    {
+        static void Main()
+        {
+            //string[] cards = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+
+            string input = Console.ReadLine();
+            Console.WriteLine(IsCard(input));
+        }
+
+        static string IsCard(string input)
+        {
+            int.TryParse(input, out int result);
+            if (result >= 2 && result <= 10)
+            {
+                return string.Format("yes {0}", input);
+            }
+            else if (input.ToUpper() == "Q")
+            {
+                return string.Format("yes {0}", input);
+            }
+            else if (input.ToUpper() == "K")
+            {
+                return string.Format("yes {0}", input);
+            }
+            else if (input.ToUpper() == "J")
+            {
+                return string.Format("yes {0}", input);
+            }
+            else if (input.ToUpper() == "A")
+            {
+                return string.Format("yes {0}", input);
+            }
+            else
+            {
+                return string.Format("no {0}", input);
+            }
+        }
+    }
+}
