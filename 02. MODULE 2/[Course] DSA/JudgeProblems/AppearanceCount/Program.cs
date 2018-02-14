99d810082ded61dd5bf50971437a10ec37d67391
Irina Hristova <hristova119@gmail.com>
2018-02-15 01:43:54 +0200

    Add solution

---
+++ 02. MODULE 2/[Course] DSA/JudgeProblems/AppearanceCount/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class AppearanceCount
+{
+    static void CountAppearences(string[] array, int number)
+    {
+        int counter = 0;
+        for (int count = 0; count < array.Length; count++)
+        {
+            if (int.Parse(array[count]) == number)
+            {
+                counter++;
+            }
+        }
+        Console.WriteLine(counter);
+    }
+    static void Main()
+    {
+        int length = int.Parse(Console.ReadLine());
+        var array = Console.ReadLine().Split(' ');
+        int number = int.Parse(Console.ReadLine());
+        CountAppearences(array, number);
+    }
+}
---
+++ 02. MODULE 2/[Course] DSA/JudgeProblems/LargerThanNeighbours/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class LargerThanNeighbours
+{
+    static void Main(string[] args)
+    {
+        int arraySize = int.Parse(Console.ReadLine());
+        string[] values = Console.ReadLine().Split(' ');
+
+        int[] arr = new int[arraySize];
+        int count = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = int.Parse(values[i]);
+        }
+
+        count = LargerThanTheNeighbours(arr, count);
+
+        Console.WriteLine(count);
+    }
+
+    static int LargerThanTheNeighbours(int[] array, int neighboursCount)
+    {
+        for (int i = 1; i < array.Length - 1; i++)
+        {
+            if (array[i] > array[i - 1] && array[i] > array[i + 1])
+            {
+                neighboursCount++;
+            }
+        }
+
+        return neighboursCount;
+    }
+}
