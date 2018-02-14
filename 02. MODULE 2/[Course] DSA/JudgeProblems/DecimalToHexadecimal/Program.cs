3d5afc78defa91b82c7242a13ea5e0089bc6345d
Irina Hristova <hristova119@gmail.com>
2018-02-15 01:27:26 +0200

    Added solution

---
+++ 02. MODULE 2/[Course] DSA/JudgeProblems/DecimalToHexadecimal/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DecimalToHexadecimal
+{
+    public class Program
+    {
+        static void Main()
+        {
+            long dec = long.Parse(Console.ReadLine());
+
+            Console.WriteLine(DecimalToHexadecimal(dec));
+        }
+
+        public static string DecimalToHexadecimal(long dec)
+        {
+            if (dec < 1) return "0";
+
+            long hex = dec;
+            string hexStr = string.Empty;
+
+            while (dec > 0)
+            {
+                hex = dec % 16;
+
+                if (hex < 10)
+                    hexStr = hexStr.Insert(0, Convert.ToChar(hex + 48).ToString());
+                else
+                    hexStr = hexStr.Insert(0, Convert.ToChar(hex + 55).ToString());
+
+                dec /= 16;
+            }
+
+            return hexStr;
+        }
+    }
+}
