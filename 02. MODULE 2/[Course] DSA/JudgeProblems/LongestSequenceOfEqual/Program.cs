3dd7d96d85dfa5f4b59dfb26150ff715f3ba5d46
Irina Hristova <hristova119@gmail.com>
2018-02-14 23:09:11 +0200

    Added solutions

---
+++ 02. MODULE 2/[Course] DSA/JudgeProblems/LongestSequenceOfEqual/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class MaxSequenceOfEqualElements
+{
+    static void Main()
+    {
+        int length = int.Parse(Console.ReadLine());
+        int[] elements = new int[length];
+        int maxCount = 0;
+        int currentCount = 1;
+        int sequenceStart = 0;
+        for (int index = 0; index < length; index++)
+        {
+            elements[index] = int.Parse(Console.ReadLine());
+            if (index != 0)
+            {
+                if (elements[index] == elements[index - 1])
+                {
+                    currentCount++;
+                }
+                else
+                {
+                    currentCount = 1;
+                }
+                if (currentCount > maxCount)
+                {
+                    maxCount = currentCount;
+                    sequenceStart = index + 1 - maxCount;
+                }
+            }
+        }
+        
+            Console.WriteLine("{0} ", maxCount);
+    }
+}
---
+++ 02. MODULE 2/[Course] DSA/JudgeProblems/SpiralMatrix/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace _01.FillTheMatrix
+{
+    using System;
+
+    class FillTheMatrix
+    {
+        public static int counter = 1;
+        public static int rows;
+        public static int cols;
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            CalculateCaseD(matrix, n);
+        }
+
+        public static void CalculateCaseD(int[,] matrix, int n)
+        {
+            rows = 0;
+            cols = 0;
+            counter = 1;
+            matrix[rows, cols] = counter;
+
+            for (int bound = 0; bound < n / 2 + 1; bound++)
+            {
+                rows = bound;
+                cols = bound;
+                for (; rows < n - bound; rows++)
+                {
+                    if (rows == n - bound - 1)
+                    {
+                        for (; cols < n - bound; cols++)
+                        {
+                            matrix[rows, cols] = counter;
+                            counter++;
+                        }
+                    }
+                    else
+                    {
+                        matrix[rows, cols] = counter;
+                        counter++;
+                    }
+                }
+                cols = n - bound - 1;
+                rows = n - bound - 2;
+                for (; rows >= bound; rows--)
+                {
+
+                    if (rows == bound)
+                    {
+                        for (cols = n - bound - 1; cols >= bound + 1; cols--)
+                        {
+                            matrix[rows, cols] = counter;
+                            counter++;
+                        }
+                    }
+                    else
+                    {
+                        matrix[rows, cols] = counter;
+                        counter++;
+                    }
+                }
+            }
+            PrintTheMatrix(matrix, n);
+        }
+
+        public static void PrintTheMatrix(int[,] matrix, int n)
+        {
+            int rows = 0, cols = 0;
+            for (cols = 0; cols < n; cols++)
+            {
+                for (rows = 0; rows < n; rows++)
+                {
+                    if (cols == n - 1)
+                    {
+                        Console.Write("{0} ", matrix[rows, cols]);
+                    }
+                    else
+                    {
+                        Console.Write("{0} ", matrix[rows, cols]);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
