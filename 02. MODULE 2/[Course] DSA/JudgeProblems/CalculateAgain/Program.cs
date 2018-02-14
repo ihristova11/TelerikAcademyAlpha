60bb90f0cef7b6ef7d1831091f9f8d84ed5257fe
Irina Hristova <hristova119@gmail.com>
2018-02-14 22:52:21 +0200

    Added solutions

---
+++ 02. MODULE 2/[Course] DSA/JudgeProblems/CalculateAgain/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+
+namespace CalculateAgain
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            BigInteger nFac = 1;
+            BigInteger kFac = 1;
+
+            for (int i = 2; i < n + 1; i++)
+            {
+                if (i <= k)
+                {
+                    kFac *= i;
+                }
+
+                nFac *= i;
+            }
+
+            Console.WriteLine(nFac / kFac);
+        }
+    }
+}
--- 02. MODULE 2/[Course] DSA/JudgeProblems/SayHello/Program.cs
+++ 02. MODULE 2/[Course] DSA/JudgeProblems/SayHello/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace SayHello
+﻿using System;
+
+namespace SayHello
 {
     public class Program
     {
         static void Main()
         {
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello, {0}!", name);
         }
     }
 }
