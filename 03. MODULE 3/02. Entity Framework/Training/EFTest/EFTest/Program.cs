736c64fc4ea6dd3544eaeeb80adec2fd93d1ec6d
Irina Hristova <hristova119@gmail.com>
2018-03-10 23:35:19 +0200

    Training

--- 03. MODULE 3/02. Entity Framework/Training/EFTest/EFTest/Program.cs
+++ 03. MODULE 3/02. Entity Framework/Training/EFTest/EFTest/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace EFTest
+﻿using System;
+using System.Linq;
+using EFTest.Data;
+
+namespace EFTest
 {
     public class Program
     {
         static void Main()
         {
+            using (var db = new TelerikAcademyEntities())
+            {
+                var project = db.Projects
+                    .FirstOrDefault(pr => pr.ProjectID == 1);
+                Console.WriteLine(project.Name);
+            }
         }
     }
 }
