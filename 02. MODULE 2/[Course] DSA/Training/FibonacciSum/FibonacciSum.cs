69611cb71731efa85ca7834936a95ca10a726ee3
Irina Hristova <hristova119@gmail.com>
2018-04-12 19:13:02 +0300

    Added FibonacciSum

--- 02. MODULE 2/[Course] DSA/Training/FibonacciSum/FibonacciSum.cs
+++ 02. MODULE 2/[Course] DSA/Training/FibonacciSum/FibonacciSum.cs
@@ -1,9 +1,19 @@
-﻿namespace FibonacciSum
+﻿using System;
+
+namespace FibonacciSum
 {
     public class FibonacciSum
     {
         static void Main()
         {
+            Console.WriteLine(Fibonacci(6));
+        }
+
+        private static int Fibonacci(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
         }
     }
 }
