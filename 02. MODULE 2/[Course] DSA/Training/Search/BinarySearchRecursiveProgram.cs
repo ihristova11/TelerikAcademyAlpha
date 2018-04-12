50319b8f19ae529abed005f8517fbdadcdf399ec
Irina Hristova <hristova119@gmail.com>
2018-04-12 13:26:20 +0300

    Binary Search Recursive

---
+++ 02. MODULE 2/[Course] DSA/Training/Search/BinarySearchRecursiveProgram.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Search
+{
+    public class BinarySearchRecursiveProgram
+    {
+        public static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 4, 6, 7, 8, 9 };
+            Console.WriteLine(BinarySearchRecursive(0, arr.Length - 1, 4, arr));
+        }
+
+        private static int BinarySearchRecursive(int left, int right, int searched, int[] arr)
+        {
+            if (left > right)
+            {
+                return -1;
+            }
+
+            int mid = (left + right) / 2;
+
+            if (searched == arr[mid])
+            {
+                return ++mid;
+            }
+            else if (searched < arr[mid])
+            {
+                right = mid - 1;
+                return BinarySearchRecursive(left, right, searched, arr);
+            }
+            else
+            {
+                left = mid + 1;
+                return BinarySearchRecursive(left, right, searched, arr);
+            }
+        }
+    }
+}
