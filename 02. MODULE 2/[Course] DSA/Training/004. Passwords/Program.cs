33c0b1d6244fec25f9c08d966a701ceb2bec5e76
Irina Hristova <hristova119@gmail.com>
2018-04-13 22:50:11 +0300

    Added sln

---
+++ 02. MODULE 2/[Course] DSA/Training/004. Passwords/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace _004.Passwords
+{
+    using System;
+
+    public class Program
+    {
+        private static char[] symbols;
+        private static int[] comb;
+        private static int n;
+        private static int k;
+        private static int counter = 0;
+
+        public static void Main()
+        {
+            var parameters = Console.ReadLine().Split(' ');
+            n = int.Parse(parameters[0]);
+            comb = new int[n];
+            symbols = parameters[1].ToCharArray();
+            k = int.Parse(parameters[2]);
+
+            for (int i = 0; i < 10; i++)
+            {
+                comb[0] = i;
+                FindCombinations(i, 0);
+            }
+        }
+
+        private static int FindCombinations(int currentDigit, int directionIndex)
+        {
+            if (directionIndex >= n)
+            {
+                return 0;
+            }
+
+            comb[directionIndex] = currentDigit;
+            if (directionIndex == n - 1)
+            {
+                counter++;
+                if (counter == k)
+                {
+                    Console.WriteLine(string.Join(string.Empty, comb).PadLeft(n, '0'));
+
+                    return 0;
+                }
+
+                return 0;
+            }
+
+            if (symbols[directionIndex] == '=')
+            {
+                FindCombinations(currentDigit, directionIndex + 1);
+                return 0;
+            }
+            else if (symbols[directionIndex] == '>')
+            {
+                if (currentDigit == 0)
+                {
+                    return 0;
+                }
+
+                FindCombinations(0, directionIndex + 1);
+
+                for (int i = currentDigit + 1; i < 10; i++)
+                {
+                    FindCombinations(i, directionIndex + 1);
+                }
+
+                return 0;
+            }
+            else
+            {
+                if (currentDigit == 0)
+                {
+                    for (int i = 1; i <= 9; i++)
+                    {
+                        FindCombinations(i, directionIndex + 1);
+                    }
+                }
+                else
+                {
+                    for (int i = 1; i < currentDigit; i++)
+                    {
+                        FindCombinations(i, directionIndex + 1);
+                    }
+                }
+
+                return 0;
+            }
+        }
+    }
+}
--- 02. MODULE 2/[Course] DSA/Training/04. Passwords/Program.cs
+++ 02. MODULE 2/[Course] DSA/Training/04. Passwords/Program.cs
@@ -3,17 +3,93 @@ using System.Linq;
 
 namespace _04.Passwords
 {
-    public class Program
+    public class Passwords
     {
-        static void Main()
+        private static char[] symbols;
+        private static int[] digits;
+        private static int[] comb;
+        private static int n;
+        private static int k;
+
+        public static void Main()
         {
-            string line = Console.ReadLine();
-            var parameters = line.Split(' ').ToArray();
-            int n = int.Parse(parameters[0]); // pass len
-            var symbols = parameters[1].ToCharArray();
-            int k = int.Parse(parameters[2]); // k-th possible pass
+            var line = Console.ReadLine().Split(' ');
+            n = int.Parse(line[0]);
+            comb = new int[n];
+            symbols = line[1].ToCharArray();
+            k = int.Parse(line[2]);
 
+            digits = Enumerable.Range(1, 10).ToArray();
+            digits[9] = 0;
 
+            Recursion(0);
+        }
+
+        private static void Recursion(int start)
+        {
+            if (n == start)
+            {
+                if (--k == 0) // only one -> to be printed
+                {
+                    Console.WriteLine(string.Join("", comb));
+                }
+
+                return;
+            }
+
+            if (start > 0)
+            {
+                if (symbols[start - 1] == '=')
+                {
+                    comb[start] = comb[start - 1];
+                    Recursion(start + 1);
+
+                }
+                else if (symbols[start - 1] == '>')
+                {
+                    var s = comb[start - 1];
+
+                    if (s == 0)
+                    {
+                        return;
+                    }
+
+                    comb[start] = 0;
+                    Recursion(start + 1);
+
+                    int j = s;
+                    if (s == 0) j = n;
+
+                    for (; j < 9; j++)
+                    {
+                        comb[start] = digits[j];
+                        Recursion(start + 1);
+                    }
+                }
+                else if (symbols[start - 1] == '<')
+                {
+                    var s = comb[start - 1];
+
+                    if (s == 0) s = 10;
+
+                    for (int j = 0; j < s - 1; j++)
+                    {
+                        comb[start] = digits[j];
+                        Recursion(start + 1);
+                    }
+                }
+            }
+            else
+            {
+                comb[start] = 0;
+                Recursion(start + 1);
+
+                for (int i = start; i < 9; i++)
+                {
+                    comb[start] = digits[i];
+                    Recursion(start + 1);
+                }
+            }
         }
     }
 }
