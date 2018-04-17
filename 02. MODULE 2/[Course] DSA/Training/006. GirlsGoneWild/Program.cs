bb0fb885a029516746787ff593f8304a5bd372f0
Irina Hristova <hristova119@gmail.com>
2018-04-17 11:45:45 +0300

    Added 006.GirlsGoneWild

---
+++ 02. MODULE 2/[Course] DSA/Training/006. GirlsGoneWild/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _006.GirlsGoneWild
+{
+    public class Program
+    {
+        private static int[] combinations;
+        private static int numberOfPeople;
+        private static char[] shirtTypes;
+
+        public static void Main()
+        {
+            int shirts = int.Parse(Console.ReadLine());
+            char[] skirts = Console.ReadLine().ToCharArray();
+            int girls = int.Parse(Console.ReadLine());
+            List<List<int>> shirtsCombinations = new List<List<int>>();
+            Skirts skirtsCombinations = new Skirts();
+        }
+
+        public class Skirts
+        {
+            List<List<char>> skirtsCombinations = new List<List<char>>();
+            HashSet<List<char>> uniqueSkirtsCombinations = new HashSet<List<char>>();
+        }
+    }
+}
