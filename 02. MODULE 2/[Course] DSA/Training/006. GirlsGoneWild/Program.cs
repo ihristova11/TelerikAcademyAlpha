8d64749359fce09e6e50dc37076db8d4d2f5f0a3
Irina Hristova <hristova119@gmail.com>
2018-04-17 12:03:40 +0300

    Working on GirlsGoneWild

--- 02. MODULE 2/[Course] DSA/Training/006. GirlsGoneWild/Program.cs
+++ 02. MODULE 2/[Course] DSA/Training/006. GirlsGoneWild/Program.cs
@@ -18,10 +18,57 @@ namespace _006.GirlsGoneWild
             Skirts skirtsCombinations = new Skirts();
         }
 
-        public class Skirts
+        private static void CombineShirts(int max, int girls, int currentShirt, List<int> currentCombination,
+            List<List<int>> shirtsCombinations)
         {
-            List<List<char>> skirtsCombinations = new List<List<char>>();
-            HashSet<List<char>> uniqueSkirtsCombinations = new HashSet<List<char>>();
+            if (currentCombination.Count == girls)
+            {
+                shirtsCombinations.Add(new List<int>(currentCombination));
+                return;
+            }
+
+            for (int i = 0; i < max; i++)
+            {
+                currentCombination.Add(i);
+                CombineShirts(max, girls, i + 1, currentCombination, shirtsCombinations);
+                currentCombination.Remove(currentCombination.Count - 1);
+            }
         }
+
+        private static void CombineSkirts(char[] skirts, int girls, HashSet<int> usedIndeces, int currentSkirtIndex,
+            List<char> currentCombination, Skirts skirtsCombination)
+        {
+            if (currentCombination.Count == girls)
+            {
+                if (!skirtsCombination.UniqueSkirtsCombinations.Contains(currentCombination))
+                {
+                    skirtsCombination.SkirtsCombinations.Add(currentCombination);
+                    skirtsCombination.UniqueSkirtsCombinations.Add(currentCombination);
+                }
+
+                return;
+            }
+
+            for (int i = 0; i < skirts.Length; i++)
+            {
+                if (usedIndeces.Contains(i))
+                {
+                    continue;
+                }
+
+                usedIndeces.Add(i);
+                char currentSkirt = skirts[i];
+                currentCombination.Add(currentSkirt);
+                CombineSkirts(skirts, girls, usedIndeces, i, currentCombination, skirtsCombination);
+                usedIndeces.Remove(i);
+                currentCombination.RemoveAt(currentCombination.Count - 1);
+            }
+        }
+    }
+
+    public class Skirts
+    {
+        public List<List<char>> SkirtsCombinations = new List<List<char>>();
+        public HashSet<List<char>> UniqueSkirtsCombinations = new HashSet<List<char>>();
     }
 }
